7449e722fbde6aaae625898b5b41c47278b59f54
Jim Mangiafico <jim@jurisdatum.com>
2021-08-24 10:09:46 -0400

    incremental improvements

--- src/parsers/coa/CourtOfAppeal.cs
+++ src/parsers/coa/CourtOfAppeal.cs
@@ -68,14 +68,11 @@ class CourtOfAppealParser : AbstractParser {
                     return header;
                 if (p.Descendants<PageBreakBefore>().Any()) // EWCA/Civ/2004/254
                     return header;
+                if (DOCX.Numbering.HasNumberOrMarker(main, p))
+                    return header;
                 if (StartsWithTitledJudgeName(p))
                     return header;
-                // if (DOCX.Numbering.HasNumberOrMarker(main, p))  // EWCA/Civ/2004/254
-                //     return header;
             }
-            // if (e is Paragraph && StartsWithTitledJudgeName(e)) {
-            //     return header;
-            // }
             AddBlock(e, header);
         }
         return null;
