4b78003b384e71c07038b43a0ed67af8231f0095
Jim Mangiafico <jim@jurisdatum.com>
2022-01-19 15:11:01 -0500

    court recognition with help of neutral citation

---
+++ src/parsers/common/enrich/CourtType2.cs
@@ -0,0 +1,76 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace UK.Gov.Legislation.Judgments.Parse {
+
+class CourtType2 {
+
+    internal static IEnumerable<IBlock> Enrich(IEnumerable<IBlock> blocks) {
+        WNeutralCitation nc = Util.Descendants<WNeutralCitation>(blocks).FirstOrDefault();
+        if (nc is null)
+            return blocks;
+        int i = 0;
+        while (i < blocks.Count()) {
+            IBlock block1 = blocks.ElementAt(i);
+            if (i < blocks.Count() - 2) {
+                IBlock block2 = blocks.ElementAt(i + 1);
+                IBlock block3 = blocks.ElementAt(i + 2);
+                List<ILine> three = Match3(block1, block2, block3, nc);
+                if (three is not null) {
+                    IEnumerable<IBlock> before = blocks.Take(i);
+                    IEnumerable<IBlock> after = blocks.Skip(i + 3);
+                    return Enumerable.Concat(Enumerable.Concat(before, three), after);
+                }
+            }
+            i += 1;
+        }
+        return blocks;
+    }
+
+    private static bool Match(Regex regex, IBlock block) {
+        if (!(block is ILine line))
+            return false;
+        if (line.Contents.Count() == 0)
+            return false;
+        IInline first = line.Contents.First();
+        if (!line.Contents.All(inline => inline is WText))
+            return false;
+        string text = line.NormalizedContent();
+        return regex.IsMatch(text);
+    }
+
+    private static List<ILine> Match3(IBlock one, IBlock two, IBlock three, WNeutralCitation nc) {
+        var re1 = new Regex(@"^IN THE HIGH COURT OF JUSTICE$", RegexOptions.IgnoreCase);
+        var re2 = new Regex(@"^BUSINESS AND PROPERTY COURTS OF ENGLAND AND WALES$", RegexOptions.IgnoreCase);
+        var re3 = new Regex(@"\d+(st|nd|rd|th)? (January|February|March|April|May|June|July|August|September|October|November|December) \d{4}$", RegexOptions.IgnoreCase);
+        if (!Match(re1, one))
+            return null;
+        if (!Match(re2, two))
+            return null;
+        if (!Match(re3, three))
+            return null;
+        Court court;
+        if (nc.Text.Contains("(Ch)"))
+            court = Courts.EWHC_Chancery_BusinessAndProperty;
+        else
+            return null;
+        return new List<ILine>(3) { Transform1(one, court), Transform1(two, court), (ILine) three };
+    }
+
+    private static WLine Transform1(IBlock block, Court court) {
+        WLine line = (WLine) block;
+        if (line.Contents.Count() == 1) {
+            WText text = (WText) line.Contents.First();
+            WCourtType ct = new WCourtType(text.Text, text.properties) { Code = court.Code };
+            return new WLine(line, new List<IInline>(1) { ct });
+        } else {
+            WCourtType2 ct = new WCourtType2() { Code = court.Code, Contents = line.Contents.Cast<WText>() };
+            return new WLine(line, new List<IInline>(1) { ct });
+        }
+    }
+
+}
+
+}
