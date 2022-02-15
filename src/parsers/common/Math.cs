811047b99921b5d3e7c7e869126e36150a6033bb
Jim Mangiafico <jim@jurisdatum.com>
2022-02-15 10:00:24 -0500

    improved import of omml2mml.xsl

--- src/parsers/common/Math.cs
+++ src/parsers/common/Math.cs
@@ -1,5 +1,6 @@
 
 using System.IO;
+using System.Reflection;
 using System.Xml;
 using System.Xml.Xsl;
 
@@ -13,14 +14,21 @@ namespace UK.Gov.Legislation.Judgments.Parse {
 
 class Math2 {
 
+    private static XslCompiledTransform xslt = new XslCompiledTransform();
+
+    static Math2() {
+        var assembly = Assembly.GetExecutingAssembly();
+        using Stream stream = assembly.GetManifestResourceStream("math.omml2mml.xsl");
+        using XmlReader reader = XmlReader.Create(stream);
+        xslt.Load(reader);
+    }
+
     internal static IMath Parse(MainDocumentPart main, OMML.OfficeMath e) {
         XmlDocument source = new XmlDocument();
         source.LoadXml(e.OuterXml);
-        XmlNodeReader reader = new XmlNodeReader(source);
+        using XmlNodeReader reader = new XmlNodeReader(source);
         XmlDocument destination = new XmlDocument();
         XmlWriter writer = destination.CreateNavigator().AppendChild();
-        XslCompiledTransform xslt = new XslCompiledTransform();
-        xslt.Load("src/parsers/common/omml2mml.xsl");
         xslt.Transform(reader, writer);
         writer.Close(); // necessary
         return new WMath(destination.DocumentElement);
