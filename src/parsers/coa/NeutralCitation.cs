0b3806e36df8269429c2b7ea2e0d1ba741bb5a80
Jim Mangiafico <jim@jurisdatum.com>
2022-03-18 10:52:22 -0400

    small improvements and bug fixes

--- src/parsers/coa/NeutralCitation.cs
+++ src/parsers/coa/NeutralCitation.cs
@@ -11,7 +11,7 @@ class NetrualCitation : Enricher2 {
 
     private static readonly string[] patterns = {
         @"^ ?Neutral Citation( Number| No)?[:\.]? *(\[\d{4}\] EWCA (Civ|Crim) \d+)",
-        @"^Neutral [Cc]itation( +[Nn]umber| No)? ?[:\.]? *(\[\d{4}\] EWHC +\d+ +\((Admin|Admlty|Ch|Comm|Costs|Fam|IPEC|Pat|QB|TCC)\.?\))",    // . in EWHC/Comm/2007/197
+        @"^Neutral [Cc]itation( +[Nn]umber| No)? ?[:\.]? *(\[\d{4}\]? EWHC +\d+ +\((Admin|Admlty|Ch|Comm|Costs|Fam|IPEC|Pat|QB|TCC)\.?\))",    // . in EWHC/Comm/2007/197
         @"^Neutral Citation( Number| No)?:? +(\[\d{4}\] EWHC \d+ (Admin|Admlty|Ch|Comm|Costs|Fam|IPEC|Pat|QB|TCC))",  // EWHC/Admin/2003/301
         @"^Neutral Citation( Number| No)?:? +(\[\d{4}\] EWCH \d+ \((Admin|Admlty|Ch|Comm|Costs|Fam|IPEC|Pat|QB|TCC)\))",   // EWHC/Admin/2006/2373
         @"^Neutral Citation( Number)?:? (\[\d{4}\] EWCOP \d+)",
