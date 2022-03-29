5ae6f7c144528d06fff3378ed8bd17d51b3cf0b8
Jim Mangiafico <jim@jurisdatum.com>
2022-03-29 11:38:51 -0400

    throw error if XML is not valid against AkN schema

---
+++ src/akn/Validator.cs
@@ -0,0 +1,37 @@
+
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace UK.Gov.Legislation.Judgments.AkomaNtoso {
+
+public class Validator {
+
+    private XmlSchemaSet Schemas = new XmlSchemaSet();
+
+    public Validator() {
+        var assembly = Assembly.GetExecutingAssembly();
+        using (Stream stream = assembly.GetManifestResourceStream("akn.akomantoso30.xsd")) {
+            using (XmlReader reader = XmlReader.Create(stream)) {
+                Schemas.Add(Builder.ns, reader);
+            }
+        }
+        using (Stream stream = assembly.GetManifestResourceStream("akn.xml.xsd")) {
+            using (XmlReader reader = XmlReader.Create(stream)) {
+                Schemas.Add("http://www.w3.org/XML/1998/namespace", reader);
+            }
+        }
+    }
+
+    public List<ValidationEventArgs> Validate(XmlDocument akn) {
+        akn.Schemas = Schemas;
+        List<ValidationEventArgs> errors = new List<ValidationEventArgs>();
+        akn.Validate((sender, e) => errors.Add(e));
+        return errors;
+    }
+
+}
+
+}
