01189dbcace6e39d7065c524eae4fbc71d013caf
Jim Mangiafico <jim@jurisdatum.com>
2022-05-23 10:40:04 -0400

    improved handling of 'Structured Document Tags' within table cells

--- test/Tests.cs
+++ test/Tests.cs
@@ -24,7 +24,7 @@ public class Tests {
     }
 
     public static IEnumerable<object[]> indices = Enumerable.Concat(
-        Enumerable.Range(1, 10), Enumerable.Range(12, 12)
+        Enumerable.Range(1, 10), Enumerable.Range(12, 13)
     ).Select(i => new object[] { i });
 
     [Theory]
