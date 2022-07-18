ab1ef728c87060424a221ae33457e2832c52cdc2
Jim Mangiafico <jim@jurisdatum.com>
2022-07-18 10:39:40 -0400

    convert tiff images to png

--- src/img/Convert.cs
+++ src/img/Convert.cs
@@ -9,6 +9,15 @@ class Convert {
 
     internal static byte[] ConvertToPng(byte[] source) {
         using var image = Image.Load(source);
+        return ConvertToPng(image);
+    }
+
+    internal static byte[] ConvertToPng(Stream source) {
+        using var image = Image.Load(source);
+        return ConvertToPng(image);
+    }
+
+    private static byte[] ConvertToPng(Image image) {
         using var stream = new MemoryStream();
         image.SaveAsPng(stream);
         return stream.ToArray();
