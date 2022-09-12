02d019fa8abbbb15c790edf26cd9e221a4746c21
Jim Mangiafico <jim@jurisdatum.com>
2022-09-12 12:55:36 -0400

    count some empty paragraphs

--- test/Tests.cs
+++ test/Tests.cs
@@ -26,7 +26,7 @@ public class Tests {
     public static IEnumerable<object[]> indices = Enumerable.Concat(
         Enumerable.Range(1, 10), Enumerable.Concat(
             Enumerable.Range(12, 16),
-            Enumerable.Range(29, 10)
+            Enumerable.Range(29, 11)
         )
     ).Select(i => new object[] { i });
 
