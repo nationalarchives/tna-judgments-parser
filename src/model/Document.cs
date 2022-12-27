95b041a29d8886c66f575498f1fcabc3fe6d8d1c
Jim Mangiafico <jim@jurisdatum.com>
2022-12-27 14:35:05 -0500

    initial work for press summaries

---
+++ src/model/Document.cs
@@ -0,0 +1,65 @@
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+
+using UK.Gov.Legislation.Judgments;
+
+namespace UK.Gov.NationalArchives.CaseLaw {
+
+enum DocType { Summary }
+
+interface IAknDocument {
+
+    DocType Type { get; }
+
+    IAknMetadata Metadata { get; }
+
+    IEnumerable<IBlock> Body { get; }
+
+    IEnumerable<IImage> Images { get; }
+
+}
+
+interface IAknMetadata {
+
+    IResource Source { get; }
+
+    IResource Author { get; }
+
+    string WorkURI { get; }
+
+    string ExpressionURI { get; }
+
+    string ManifestationURI { get => ExpressionURI + "/data.xml"; }
+
+    INamedDate Date { get; }
+
+    string? Name { get; }
+
+    string? ProprietaryNamespace { get; }
+
+    IList<Tuple<String, String>> Proprietary { get; }
+
+    IDictionary<string, IDictionary<string, string>> CSSStyles { get; }
+
+}
+
+enum ResourceType { Oranization, Event, Person, Role, Concept }
+
+interface IResource {
+
+    ResourceType Type { get; }
+
+    string ID { get; }
+
+    string URI { get; }
+
+    string? ShowAs { get; }
+
+    string? ShortForm { get; }
+
+}
+
+}
