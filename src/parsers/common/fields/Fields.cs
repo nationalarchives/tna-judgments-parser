bf90054c05052ef8bb1e90760c7e600d992d9130
Jim Mangiafico <jim@jurisdatum.com>
2023-02-28 12:11:20 -0500

    Index Entry (XE) fields

--- src/parsers/common/fields/Fields.cs
+++ src/parsers/common/fields/Fields.cs
@@ -311,8 +311,13 @@ class Fields {
             return RestOptional(main, withinField, i);
         }
 
+        // "The XE field is formatted as hidden text and displays no result in the document." https://support.microsoft.com/en-us/office/field-codes-xe-index-entry-field-abaf7c78-6e21-418d-bf8b-f8186d2e4d08
+        if (fieldCode.StartsWith(" XE ")) { // [2023] EWHC 424 (TCC)
+            return Enumerable.Empty<IInline>();
+        }
+
         // https://support.microsoft.com/en-us/office/list-of-field-codes-in-word-1ad6d91a-55a7-4a8d-b535-cf7888659a51
-        throw new Exception();
+        throw new Exception(fieldCode);
     }
 
     internal static IEnumerable<IInline> Rest(MainDocumentPart main, IEnumerable<OpenXmlElement> rest) {
