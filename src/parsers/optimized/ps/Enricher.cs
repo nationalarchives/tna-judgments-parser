cdf71d4c11e15108b853561ecaeba670b5a4a9d0
Jim Mangiafico <jim@jurisdatum.com>
2023-03-20 10:56:45 -0400

    doc type before date, better cite recognition

--- src/parsers/optimized/ps/Enricher.cs
+++ src/parsers/optimized/ps/Enricher.cs
@@ -16,9 +16,14 @@ class PressSummaryEnricher {
         return new PressSummaryEnricher(blocks).Enrich();
     }
 
-    enum State { BeforeDate, AfterDateBeforeDocType, AfterDocTypeBeforeCite, Done };
+    enum State {
+        Start,
+        AfterDateBeforeDocType, AfterDocTypeBeforeDate, // date and docType can be in either order
+        AfterDateAndDocTypeBeforeCite,                  // cite is always after both date and docType
+        Done                                            // haven't necessarily found all but stop looking
+    };
 
-    private State state = State.BeforeDate;
+    private State state = State.Start;
 
     internal readonly IEnumerable<IBlock> Blocks;
 
@@ -35,7 +40,7 @@ class PressSummaryEnricher {
     private IEnumerable<IBlock> Enrich() {
         while (Enumerator.MoveNext()) {
             IBlock block = Enumerator.Current;
-            if (state == State.BeforeDate) {
+            if (state == State.Start) {
                 if (block is not WLine line) {
                     return Blocks;
                 }
@@ -59,6 +64,12 @@ class PressSummaryEnricher {
                     state = State.AfterDateBeforeDocType;
                     continue;
                 }
+                enriched1 = EnrichDocType(line);
+                if (!Object.ReferenceEquals(enriched1, line)) {
+                    Enriched.Add(enriched1);
+                    state = State.AfterDocTypeBeforeDate;
+                    continue;
+                }
                 return Blocks;
             }
             if (state == State.AfterDateBeforeDocType) {
@@ -75,14 +86,35 @@ class PressSummaryEnricher {
                 WLine enriched1 = EnrichDocType(line);
                 if (!Object.ReferenceEquals(enriched1, line)) {
                     Enriched.Add(enriched1);
-                    state = State.AfterDocTypeBeforeCite;
+                    state = State.AfterDateAndDocTypeBeforeCite;
                     continue;
                 }
                 Enriched.Add(line);
                 state = State.Done;
                 continue;
             }
-            if (state == State.AfterDocTypeBeforeCite) {
+            if (state == State.AfterDocTypeBeforeDate) {
+                if (block is not WLine line) {
+                    Enriched.Add(block);
+                    state = State.Done;
+                    continue;
+                }
+                if (IsRed(line)) {
+                    WRestriction restriction = new WRestriction(line);
+                    Enriched.Add(restriction);
+                    continue;
+                }
+                WLine enriched1 = EnrichDate(line);
+                if (!Object.ReferenceEquals(enriched1, line)) {
+                    Enriched.Add(enriched1);
+                    state = State.AfterDateAndDocTypeBeforeCite;
+                    continue;
+                }
+                Enriched.Add(line);
+                state = State.Done;
+                continue;
+            }
+            if (state == State.AfterDateAndDocTypeBeforeCite) {
                 if (block is not WLine line) {
                     Enriched.Add(block);
                     state = State.Done;
@@ -154,35 +186,68 @@ class PressSummaryEnricher {
     }
 
     private WLine EnrichCite(WLine line) {
-        WLine enriched = EnrichLastText(line);
-        if (!Object.ReferenceEquals(enriched, line))
-            return enriched;
-        return EnrichWholeLine(line);
+        // WLine enriched = EnrichLastText(line);
+        // if (!Object.ReferenceEquals(enriched, line))
+        //     return enriched;
+        // enriched = EnrichFromEnd(line);
+        // if (!Object.ReferenceEquals(enriched, line))
+        //     return enriched;
+        // return EnrichWholeLine(line);
+        return EnrichFromEnd(line);
     }
 
-    private static string pattern1 = @"(\[\d{4}\] (UKSC|UKPC) \d+) *$";
+    private static string pattern1 = @"(\[\d{4}\] (UKSC|UKPC) \d+)\.? *$";
 
-    private static WLine EnrichLastText(WLine line) {
-        if (line.Contents.LastOrDefault() is not WText text)
-            return line;
-        Match match = Regex.Match(text.Text, pattern1);
-        if (!match.Success)
-            return line;
-        List<IInline> replacement = Helper.SplitOnGroup(text, match.Groups[1], (txt, rProps) => new WNeutralCitation(txt, rProps));
-        return WLine.Make(line, Enumerable.Concat( line.Contents.SkipLast(1), replacement ));
+    // private static WLine EnrichLastText(WLine line) {
+    //     if (line.Contents.LastOrDefault() is not WText text)
+    //         return line;
+    //     Match match = Regex.Match(text.Text, pattern1);
+    //     if (!match.Success)
+    //         return line;
+    //     List<IInline> replacement = Helper.SplitOnGroup(text, match.Groups[1], (txt, rProps) => new WNeutralCitation(txt, rProps));
+    //     return WLine.Make(line, Enumerable.Concat( line.Contents.SkipLast(1), replacement ));
+    // }
+
+    private static WLine EnrichFromEnd(WLine line) {
+        IEnumerator<IInline> reversed = line.Contents.Reverse().GetEnumerator();
+        string end = "";
+        while (reversed.MoveNext()) {
+            if (reversed.Current is not WText wText)
+                return line;
+            end = wText.Text + end;
+            Match match = Regex.Match(end, pattern1);
+            if (match.Success) {
+                List<IInline> replacement = new List<IInline>();
+                Group group = match.Groups[1];
+                if (group.Index > 0) {
+                    WText leading = new WText(end.Substring(0, group.Index), wText.properties);
+                    replacement.Add(leading);
+                }
+                IInline middle = new WNeutralCitation(group.Value, wText.properties);
+                replacement.Add(middle);
+                if (group.Index + group.Length < end.Length) {
+                    WText trailing = new WText(end.Substring(group.Index + group.Length), (line.Contents.Last() as WText).properties);
+                    replacement.Add(trailing);
+                }
+                while (reversed.MoveNext())
+                    replacement.Insert(0, reversed.Current);
+                return WLine.Make(line, replacement);
+            }
+        }
+        return line;
     }
 
-    private static string pattern2 = @"^\[\d{4}\] (UKSC|UKPC) \d+$";
+    // private static string pattern2 = @"^\[\d{4}\] (UKSC|UKPC) \d+$";
 
-    private static WLine EnrichWholeLine(WLine line) {
-        if (!line.Contents.All(inline => inline is IFormattedText))
-            return line;
-        Match match = Regex.Match(line.NormalizedContent, pattern2);
-        if (!match.Success)
-            return line;
-        WNeutralCitation2 ncn = new WNeutralCitation2 { Contents = line.Contents.Cast<IFormattedText>() };
-        return WLine.Make(line, new List<IInline>(1) { ncn });
-    }
+    // private static WLine EnrichWholeLine(WLine line) {
+    //     if (!line.Contents.All(inline => inline is IFormattedText))
+    //         return line;
+    //     Match match = Regex.Match(line.NormalizedContent, pattern2);
+    //     if (!match.Success)
+    //         return line;
+    //     WNeutralCitation2 ncn = new WNeutralCitation2 { Contents = line.Contents.Cast<IFormattedText>() };
+    //     return WLine.Make(line, new List<IInline>(1) { ncn });
+    // }
 
 }
 
