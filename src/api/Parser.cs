f07944328a1ec328605cf5ac132d8495e3818047
Jim Mangiafico <jim@jurisdatum.com>
2023-07-19 18:29:26 -0400

    remove PS from API

--- src/api/Parser.cs
+++ src/api/Parser.cs
@@ -154,9 +154,9 @@ public class Parser {
             }
         }
 
-        PressSummary ps = PressSummaryParser.Parse(doc, preParsed);
-        if (PressSummary.Score(ps) == PressSummary.PerfectScore)
-            return ps.Bundle();
+        // PressSummary ps = PressSummaryParser.Parse(doc, preParsed);
+        // if (PressSummary.Score(ps) == PressSummary.PerfectScore)
+        //     return ps.Bundle();
 
         return new AkN.Bundle(doc, judgment1);
     }
