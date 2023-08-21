d679c704732744fd70fa4c06ca97f0de88bd8c46
Jim Mangiafico <jim@jurisdatum.com>
2023-08-21 09:29:00 -0400

    ANSI characters

--- src/parsers/common/fields/Symbol.cs
+++ src/parsers/common/fields/Symbol.cs
@@ -1,13 +1,12 @@
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
+
 
 namespace UK.Gov.Legislation.Judgments.Parse.Fieldss {
 
@@ -16,18 +15,41 @@ namespace UK.Gov.Legislation.Judgments.Parse.Fieldss {
 class Symbol {
 
     private static readonly string pattern = @"^ SYMBOL (183) \\f ""(Symbol)"" \\s (\d+) \\h $";
+    private static readonly string pattern2 = @"^ SYMBOL (\d+) \\\* MERGEFORMAT ";
+
+    private static readonly Encoding ANSI;
+
+
+    static Symbol() {
+        // Win ANSI encoding not registered by default in on .NET Core
+        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        ANSI = Encoding.GetEncoding(1252);
+    }
 
     internal static IInline Parse(MainDocumentPart main, string fieldCode, IEnumerable<OpenXmlElement> rest) {
         Match match = Regex.Match(fieldCode, pattern);
-        if (!match.Success)
-            throw new Exception();
-        string code = match.Groups[1].Value;
-        string font = match.Groups[2].Value;
-        float points = float.Parse(match.Groups[3].Value);
-        int utf32 = int.Parse(code);    // NumberStyles.AllowHexSpecifier doesn't seem to work here
-        string symbol = Char.ConvertFromUtf32(utf32);
-        return new SpecialCharacter(symbol, null, font, points);
+        if (match.Success) {
+            string code = match.Groups[1].Value;
+            string font = match.Groups[2].Value;
+            float points = float.Parse(match.Groups[3].Value);
+            int utf32 = int.Parse(code);    // NumberStyles.AllowHexSpecifier doesn't seem to work here
+            string symbol = Char.ConvertFromUtf32(utf32);
+            return new SpecialCharacter(symbol, null, font, points);
+        }
+        match = Regex.Match(fieldCode, pattern2);
+        if (match.Success) {
+            byte ansi = byte.Parse(match.Groups[1].Value);
+            string utf8 = AnsiToUtf8(ansi);
+            return new WText(utf8, null);
+        }
+        throw new Exception();
     }
+
+    private static string AnsiToUtf8(byte ansi) {
+        byte[] utf8 = Encoding.Convert(ANSI, Encoding.UTF8, new byte[] { ansi });
+        return Encoding.UTF8.GetString(utf8);
+    }
+
 }
 
 }
