6b70590ab7ca2b497994bacf41fe97a43b83b7a7
Jim Mangiafico <jim@jurisdatum.com>
2023-08-30 13:33:18 -0400

    em tests

--- src/leg/em/Helper.cs
+++ src/leg/em/Helper.cs
@@ -1,25 +1,31 @@
 
 using System.IO;
 using System.Xml;
+
 using DocumentFormat.OpenXml.Packaging;
+
+using UK.Gov.NationalArchives.AkomaNtoso;
 
 namespace UK.Gov.Legislation.ExplanatoryMemoranda {
 
 class Helper {
 
-    public static IXmlDocument Parse(Stream docx) {
+    public static IXmlDocument Parse(Stream docx, bool simplify = true) {
         WordprocessingDocument word = UK.Gov.Legislation.Judgments.AkomaNtoso.Parser.Read(docx);
-        return Parse(word);
+        return Parse(word, simplify);
     }
 
-    public static IXmlDocument Parse(byte[] docx) {
+    public static IXmlDocument Parse(byte[] docx, bool simplify = true) {
         WordprocessingDocument word = UK.Gov.Legislation.Judgments.AkomaNtoso.Parser.Read(docx);
-        return Parse(word);
+        return Parse(word, simplify);
     }
 
-    private static IXmlDocument Parse(WordprocessingDocument docx) {
+    private static IXmlDocument Parse(WordprocessingDocument docx, bool simplify) {
         IDocument doc = ExplanatoryMemoranda.Parser.Parse(docx);
         XmlDocument xml = Builder.Build(doc);
+        docx.Close();
+        if (simplify)
+            Simplifier.Simplify(xml);
         return new XmlDocument_ { Document = xml };
     }
 
