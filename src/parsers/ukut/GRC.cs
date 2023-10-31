de1172d9c3cc0c44dd395366f2c5808b05965cdc
Jim Mangiafico <jim@jurisdatum.com>
2023-10-31 13:04:01 -0400

    improve GRC jurisdictions

--- src/parsers/ukut/GRC.cs
+++ src/parsers/ukut/GRC.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
+using Microsoft.Extensions.Logging;
+
 using UK.Gov.Legislation.Judgments;
 using UK.Gov.Legislation.Judgments.Parse;
 
@@ -10,9 +12,13 @@ namespace UK.Gov.NationalArchives.CaseLaw.Parsers.UKUT {
 
 class GRCCombo : Combo3 {
 
-    public new static Regex Re1 => new(@"^First-tier Tribunal$", RegexOptions.IgnoreCase);
-    public new static Regex Re2 => new(@"^\(?General Regulatory Chamber\)?$", RegexOptions.IgnoreCase);
-    public new static Court Court => Courts.FirstTierTribunal_GRC;
+    private static readonly ILogger Logger = Logging.Factory.CreateLogger<GRCCombo>();
+
+    internal GRCCombo() {
+        Re1 = new(@"^First-tier Tribunal$", RegexOptions.IgnoreCase);
+        Re2 = new(@"^\(?General Regulatory Chamber\)?$", RegexOptions.IgnoreCase);
+        Court = Courts.FirstTierTribunal_GRC;
+    }
 
     override internal bool Match(IBlock one, IBlock two, IBlock three) {
         return Match(Re1, one) && Match(Re2, two) && MatchJurisdiction(three);
@@ -37,227 +43,166 @@ class GRCCombo : Combo3 {
 
 
     private static bool MatchJurisdiction(IBlock block) {
-        // if (block is not WLine line)
-        //     return false;
-        // string text = line.NormalizedContent.TrimStart('(').TrimEnd(')');
-        // foreach (var x in X)
-        //     foreach (string pattern in x.Patterns)
-        //         if (text.StartsWith(pattern, StringComparison.InvariantCultureIgnoreCase))
-        //             return true;
-        // return false;
         return GetJurisdiction(block) is not null;
     }
 
     private static GRCJurisdiction GetJurisdiction(IBlock block) {
         if (block is not WLine line)
             return null;
-        string text = line.NormalizedContent.TrimStart('(').TrimEnd(')');
-        foreach (var x in X)
-            foreach (string pattern in x.Patterns)
+        string text = line.NormalizedContent.TrimStart('(').TrimStart('[');
+        foreach (var jd in Jurisdictions)
+            foreach (string pattern in jd.Patterns)
                 if (text.StartsWith(pattern, StringComparison.InvariantCultureIgnoreCase))
-                    return x;
+                    return jd;
         return null;
     }
 
     private static WLine TransformJurisdiction(IBlock block) {
         WLine line = (WLine) block;
-        GRCJurisdiction x = GetJurisdiction(block);
-        WDocJurisdiction juris = new() { Contents = line.Contents, LongName = x.Name, ShortName = x.Abbreviation };
+        GRCJurisdiction jd = GetJurisdiction(block);
+        Logger.LogInformation("found jurisdiction {}", jd.Abbreviation);
+        WDocJurisdiction juris = new() { Contents = line.Contents, LongName = jd.LongName, ShortName = jd.Abbreviation };
         return WLine.Make(line, new List<IInline>(1) { juris });
     }
-
-    // static readonly IDictionary<string, ISet<string>> Jurisdictions = new Dictionary<string, ISet<string>>() {
-    //     { "Charity", new HashSet<string>() { "Charity" } },
-    //     { "InfoRights", new HashSet<string>() { "Information Rights" } },
-    //     { "AnimalWelfare", new HashSet<string>() { "Welfare of animals" } }  
-    // };
 
     // https://www.judiciary.uk/courts-and-tribunals/tribunals/first-tier-tribunal/general-regulatory-chamber/the-work-of-the-general-regulatory-chamber/
     // https://www.gov.uk/courts-tribunals/first-tier-tribunal-general-regulatory-chamber
     // https://sway.office.com/xyqXZSFJRCZIo1DV?ref=Link
 
-
-/*
-
-Charity,
-Community Right to Bid,
-Electronic Communications, Postal Services and Network Information Systems,
-Environment,
-Estate Agents,
-Exam Boards,
-Food Safety,
-Gambling,
-Immigration Services,
-Individual Electoral Regulation,
-Information Rights,
-Pensions,
-Standards & Licensing,
-Transport, and
-Welfare of Animals.
-
-*/
-
-
-
-    static readonly GRCJurisdiction[] X = {
+    static readonly GRCJurisdiction[] Jurisdictions = {
         new() {
-            Name = "Charity",
+            LongName = "Charity",
             Abbreviation = "Charity",
             Patterns = new string[] { "Charity", "Charities" },
             Prefix = "CA"
         },
+        // new() {
+        //     Name = "Claims Management",
+        //     Abbreviation = "Claims-Management",
+        //     Patterns = new string[] { "Claims" },
+        //     Prefix = null
+        // },
         new() {
-            Name = "Claims Management",
-            Abbreviation = "Claims-Management",
-            Patterns = new string[] { "Claims" },
-            Prefix = null
-        },
-        new() {
-            Name = "Community Right to Bid", // "Community Rights"
-            Abbreviation = "Community-Right-to-Bid", // "Community-Rights"
+            LongName = "Community Right to Bid",
+            Abbreviation = "CommunityRights",
             Patterns = new string[] { "Community", "Right to Bid" },
             Prefix = "CR"
         },
+        // new() {
+        //     Name = "Consultant Lobbyists",
+        //     Abbreviation = "Consultant-Lobbyists",
+        //     Patterns = new string[] { "Consultant", "lobbyists" },
+        //     Prefix = null
+        // },
+        // new() {
+        //     Name = "Conveyancing",
+        //     Abbreviation = "Conveyancing",
+        //     Patterns = new string[] { "Conveyancing" },
+        //     Prefix = null
+        // },
+        // new() {
+        //     Name = "Copyright Licensing",
+        //     Abbreviation = "CopyrightLicensing",
+        //     Patterns = new string[] { "Copyright" },
+        //     Prefix = null
+        // },
+        // new() {
+        //     Name = "Electronic communications and postal services",
+        //     Abbreviation = "",
+        //     Patterns = new string[] { "Electronic communications", "postal services" },
+        //     Prefix = null
+        // },
         new() {
-            Name = "Consultant Lobbyists",
-            Abbreviation = "Consultant-Lobbyists",
-            Patterns = new string[] { "Consultant", "lobbyists" },
-            Prefix = null
-        },
-        new() {
-            Name = "Conveyancing",
-            Abbreviation = "Conveyancing",
-            Patterns = new string[] { "Conveyancing" },
-            Prefix = null
-        },
-        new() {
-            Name = "Copyright Licensing",
-            Abbreviation = "Copyright-Licensing",
-            Patterns = new string[] { "Copyright" },
-            Prefix = null
-        },
-        new() {
-            Name = "Electronic communications and postal services",
-            Abbreviation = "",
-            Patterns = new string[] { "Electronic communications", "postal services" },
-            Prefix = null
-        },
-        new() {
-            Name = "Environment",
+            LongName = "Environment",
             Abbreviation = "Environment",
             Patterns = new string[] { "Environment" },
             Prefix = "NV" // NVZ
         },
+        // new() {
+        //     Name = "Estate Agents",
+        //     Abbreviation = "Estate-Agents",
+        //     Patterns = new string[] { "Estate agent" },
+        //     Prefix = null
+        // },
+        // new() {
+        //     Name = "Examination Boards",
+        //     Abbreviation = "Exam-Boards",
+        //     Patterns = new string[] { "Examination boards", "Exam boards" },
+        //     Prefix = null
+        // },
+        // new() {
+        //     Name = "Food Labelling",
+        //     Abbreviation = "Food",
+        //     Patterns = new string[] { "Food labelling", "food safety" },
+        //     Prefix = null
+        // },
+        // new() {
+        //     Name = "Gambling",
+        //     Abbreviation = "Gambling",
+        //     Patterns = new string[] { "Gambling" },
+        //     Prefix = null
+        // },
+        // new() {
+        //     LongName = "Immigration Services",
+        //     Abbreviation = "ImmigrationServices",
+        //     Patterns = new string[] { "Immigration services" },
+        //     Prefix = null
+        // },
+        // new() {
+        //     Name = "Individual Electoral Registration",
+        //     Abbreviation = "Individual-Electoral-Registration",
+        //     Patterns = new string[] { "individual electoral registration" }, // Individual Electronic
+        //     Prefix = null
+        // },
         new() {
-            Name = "Estate Agents",
-            Abbreviation = "Estate-Agents",
-            Patterns = new string[] { "Estate agent" },
-            Prefix = null
-        },
-        new() {
-            Name = "Examination Boards",
-            Abbreviation = "Exam-Boards",
-            Patterns = new string[] { "Examination boards", "Exam boards" },
-            Prefix = null
-        },
-        new() {
-            Name = "Food Labelling",
-            Abbreviation = "Food",
-            Patterns = new string[] { "Food labelling", "food safety" },
-            Prefix = null
-        },
-        new() {
-            Name = "Gambling",
-            Abbreviation = "Gambling",
-            Patterns = new string[] { "Gambling" },
-            Prefix = null
-        },
-        new() {
-            Name = "Immigration Services",
-            Abbreviation = "Immigration-Services",
-            Patterns = new string[] { "Immigration services" },
-            Prefix = null
-        },
-        new() {
-            Name = "Individual Electoral Registration",
-            Abbreviation = "Individual-Electoral-Registration",
-            Patterns = new string[] { "individual electoral registration" }, // Individual Electronic
-            Prefix = null
-        },
-        new() {
-            Name = "Information Rights",
-            Abbreviation = "InfoRights",
+            LongName = "Information Rights",
+            Abbreviation = "InformationRights",
             Patterns = new string[] { "Information Rights", "Section 166 DPA 1998" },
             Prefix = "EA"
         },
+        // new() {
+        //     Name = "Letting and Managing agents",
+        //     Abbreviation = "Letting and managing Agents",
+        //     Patterns = new string[] { "Letting and managing agents", "Letting", "managing agent" },
+        //     Prefix = null
+        // },
+        // new() {
+        //     Name = "Local Government Standards",
+        //     Abbreviation = "LocalGovernment",
+        //     Patterns = new string[] { "Local government standards" },
+        //     Prefix = null
+        // },
         new() {
-            Name = "Letting and Managing agents",
-            Abbreviation = "Letting and managing Agents",
-            Patterns = new string[] { "Letting and managing agents", "Letting", "managing agent" },
-            Prefix = null
-        },
-        new() {
-            Name = "Local Government Standards",
-            Abbreviation = "Local Government",
-            Patterns = new string[] { "Local government standards" },
-            Prefix = null
-        },
-        new() {
-            Name = "Pensions Regulation",
+            LongName = "Pensions Regulation",
             Abbreviation = "Pensions",
             Patterns = new string[] { "Pensions Regulation", "Pensions" },
             Prefix = "PEN"
         },
+        // new() {
+        //     Name = "Secondary Ticketing",
+        //     Abbreviation = "SecondaryTicketing",
+        //     Patterns = new string[] { "Secondary ticketing" },
+        //     Prefix = null
+        // },
         new() {
-            Name = "Secondary Ticketing",
-            Abbreviation = "Secondary ticketing",
-            Patterns = new string[] { "Secondary ticketing" },
+            LongName = "Transport",
+            Abbreviation = "Transport",
+            Patterns = new string[] { "Transport" },
             Prefix = null
         },
         new() {
-            Name = "Welfare of Animals",
+            LongName = "Welfare of Animals",
             Abbreviation = "AnimalWelfare",
-            Patterns = new string[] { "Welfare of animals" },
+            Patterns = new string[] { "Welfare of animals", "Animal Welfare" },
             Prefix = "WA"
         }
     };
-
-
-
-    // static readonly IDictionary<string, string> Jurisdictions3 = new Dictionary<string, string>() {
-    //     { "charity", "Charity" },
-    //     { "claims", "Claims" },
-    //     { "community right to bid", "CommunityRightToBid" }
-    // };
-
-    // static readonly string[] Jurisdictions = {
-    //     "Charity",
-    //     "Claims management",
-    //     "Community right to bid",
-    //     "Consultant lobbyists",
-    //     "Conveyancing",
-    //     "Copyright licensing",
-    //     "Electronic communications and postal services",
-    //     "Environment",
-    //     "Estate agent",
-    //     "Examination boards",
-    //     "Food labelling",
-    //     "Gambling Appeals",
-    //     "Immigration services",
-    //     "Individual Electronic",
-    //     "Information rights",
-    //     "Letting and managing agents",
-    //     "Local government standards",
-    //     "Pensions Regulation",
-    //     "Secondary ticketing",
-    //     "Welfare of animals"
-    // };
 
 }
 
 class GRCJurisdiction {
 
-    internal string Name { get; init; }
+    internal string LongName { get; init; }
 
     internal string Abbreviation { get; init; }
 
