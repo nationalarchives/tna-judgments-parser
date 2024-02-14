d3ce50eda1af50f3cb8bec844a4b527246b2fb96
Jim Mangiafico <jim@jurisdatum.com>
2024-02-14 13:14:08 -0500

    no ids within embeddedStructures

--- src/akn/JudgmentBuilder.cs
+++ src/akn/JudgmentBuilder.cs
@@ -1,21 +1,46 @@
 
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Xml;
 
 using UK.Gov.Legislation.Judgments;
 using UK.Gov.Legislation.Judgments.AkomaNtoso;
+using UK.Gov.Legislation.Judgments.Parse;
 
 namespace UK.Gov.NationalArchives.CaseLaw {
 
 class JudgmentBuilder : Builder {
 
-    private HashSet<string> Ids = new HashSet<string>();
-
     override protected string UKNS => Metadata.ukns;
 
     override protected string MakeDivisionId(IDivision division) {
+        int hash = division.GetHashCode();
+        DivHashToId.TryGetValue(hash, out string id);
+        return id;
+    }
+
+    public static XmlDocument Build(IJudgment judgment) {
+        JudgmentBuilder builder = new(judgment);
+        builder.Build1(judgment);
+        AddHash(builder.doc, builder.UKNS);
+        return builder.doc;
+    }
+
+    private readonly IJudgment Judgment;
+
+    private JudgmentBuilder(IJudgment judgment) {
+        Judgment = judgment;
+        foreach (IDecision decision in judgment.Body)
+            GenerateIds(decision.Contents);
+    }
+
+    /* generate ids */
+
+    private readonly HashSet<string> UsedParagraphIds = new();
+
+    private string MakeParagraphnId(IDivision division) {
         if (division.Name != "paragraph")
             return null;
         if (division.Number is null)
@@ -24,17 +49,29 @@ class JudgmentBuilder : Builder {
         if (!match.Success)
             return null;
         string id = "para_" + match.Groups[1].Value;
-        bool isNew = this.Ids.Add(id);
+        bool isNew = this.UsedParagraphIds.Add(id);
         if (!isNew)
             return null;
         return id;
     }
 
-    public static XmlDocument Build(IJudgment judgment) {
-        JudgmentBuilder akn = new JudgmentBuilder();
-        akn.Build1(judgment);
-        AddHash(akn.doc, akn.UKNS);
-        return akn.doc;
+    readonly Dictionary<int, string> DivHashToId = new();
+
+    private void GenerateIds(IEnumerable<IDivision> divisions, string parent = null) {
+        foreach (var div in divisions) {
+            int hash = div.GetHashCode();
+            string para = MakeParagraphnId(div);
+            if (para is not null) {
+                DivHashToId.Add(hash, para);
+                continue;
+            }
+            if (div.Name == "paragraph")
+                continue;
+            if (div is not IBranch branch)
+                continue;
+            GenerateIds(branch.Children, null);
+        }
+
     }
 
 }
