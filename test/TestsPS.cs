5925d3efbc14667b53a8fcaf391059bb6a6a4831
Jim Mangiafico <jim@jurisdatum.com>
2024-02-21 13:25:00 -0500

    adjust PS tests

--- test/TestsPS.cs
+++ test/TestsPS.cs
@@ -20,12 +20,19 @@ public class TestsPS {
     [Theory]
     [MemberData(nameof(indices))]
     public void Test(int i) {
-        var docx = Tests.ReadDocx($"test.ps.test{i}.docx");
-        var expected = Tests.ReadXml($"test.ps.test{i}.xml");
+        var docx = ReadDocx(i);
+        var expected = ReadXml(i);
         var actual = Api.Parser.Parse(new Api.Request { Content = docx }).Xml;
         actual = main.RemoveSomeMetadata(actual);
         expected = main.RemoveSomeMetadata(expected);
         Assert.Equal(expected, actual);
+    }
+
+    public static byte[] ReadDocx(int i) {
+        return Tests.ReadDocx($"test.ps.test{i}.docx");
+    }
+    public static string ReadXml(int i) {
+        return Tests.ReadXml($"test.ps.test{i}.xml");
     }
 
 }
