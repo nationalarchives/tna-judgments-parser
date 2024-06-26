ce82374f8017efe64c348d425508700596404957
Jim Mangiafico <jim@jurisdatum.com>
2024-06-26 12:53:21 -0400

    fixed numbering bug affecting [2024] EWFC 147 (B)

--- test/Tests.cs
+++ test/Tests.cs
@@ -23,7 +23,7 @@ public class Tests {
         Transform.Load(xsltReader);
     }
 
-    static readonly int total = 86;
+    static readonly int total = 87;
 
     public static readonly IEnumerable<object[]> indices =
         Enumerable.Concat(
