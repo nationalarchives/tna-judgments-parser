5766b695c5df9fc1c5baa15cfae220dd612bbe01
Jim Mangiafico <jim@jurisdatum.com>
2024-07-13 15:08:27 -0400

    numbering fix for ewhc/ch/2024/1725

--- test/Tests.cs
+++ test/Tests.cs
@@ -23,7 +23,7 @@ public class Tests {
         Transform.Load(xsltReader);
     }
 
-    static readonly int total = 88;
+    static readonly int total = 89;
 
     public static readonly IEnumerable<object[]> indices =
         Enumerable.Concat(
