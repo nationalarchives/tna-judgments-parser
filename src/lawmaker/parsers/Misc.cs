807b0f8709275b5f17e7b5dcdb51319f1a5f56be
Jim Mangiafico <jim@jurisdatum.com>
2025-02-07 13:44:18 -0500

    better table grouping

--- src/lawmaker/parsers/Misc.cs
+++ src/lawmaker/parsers/Misc.cs
@@ -13,12 +13,14 @@ namespace UK.Gov.Legislation.Lawmaker
     {
 
         private IBlock Current() => Document.Body[i].Block;
-        private static bool IsLeftAligned(WLine line) {
+        private static bool IsLeftAligned(WLine line)
+        {
             var alignment = line.GetEffectiveAlignment();
             return !alignment.HasValue || alignment == AlignmentValues.Left || alignment == AlignmentValues.Justify;
         }
 
-        private static bool IsCenterAligned(WLine line) {
+        private static bool IsCenterAligned(WLine line)
+        {
             var alignment = line.GetEffectiveAlignment();
             return alignment == AlignmentValues.Center;
         }
@@ -27,27 +29,32 @@ namespace UK.Gov.Legislation.Lawmaker
 
         private static float GetEffectiveIndent(WLine line) => OptimizedParser.GetEffectiveIndent(line);
 
-        private bool CurrentLineIsIndentedLessThan(WLine parent) {
+        private bool CurrentLineIsIndentedLessThan(WLine parent)
+        {
             if (Document.Body[i].Block is not WLine line)
                 return false;
             return LineIsIndentedLessThan(line, parent);
         }
 
-        private static bool LineIsIndentedLessThan(WLine line, WLine other) {
+        private static bool LineIsIndentedLessThan(WLine line, WLine other)
+        {
             return GetEffectiveIndent(line) < GetEffectiveIndent(other);
         }
 
-        private static bool LineIsIndentedMoreThan(WLine line, WLine other) {
+        private static bool LineIsIndentedMoreThan(WLine line, WLine other)
+        {
             return GetEffectiveIndent(line) > GetEffectiveIndent(other);
         }
 
-        private static bool NextChildIsAcceptable(List<IDivision> children, IDivision next) {
+        private static bool NextChildIsAcceptable(List<IDivision> children, IDivision next)
+        {
             if (children.Count == 0)
                 return true;
             IDivision prev = children.Last();
             if (prev is Prov1 != next is Prov1)
                 return false;
-            if (prev is Prov1 && next is Prov1) {
+            if (prev is Prov1 && next is Prov1)
+            {
                 string num1 = prev.Number.Text.TrimEnd('.');
                 string num2 = next.Number.Text.TrimEnd('.');
                 if (num1.All(char.IsDigit) && num2.All(char.IsDigit))
@@ -55,7 +62,8 @@ namespace UK.Gov.Legislation.Lawmaker
             }
             if (prev is Prov2 != next is Prov2)
                 return false;
-            if (prev is Prov2 && next is Prov2) {
+            if (prev is Prov2 && next is Prov2)
+            {
                 string num1 = prev.Number.Text.TrimStart('(').TrimEnd(')');
                 string num2 = next.Number.Text.TrimStart('(').TrimEnd(')');
                 if (num1.All(char.IsDigit) && num2.All(char.IsDigit))
@@ -63,7 +71,8 @@ namespace UK.Gov.Legislation.Lawmaker
             }
             // if (prev is Para1 != next is Para1)
             //     return false;
-            if (prev is Para1 && next is Para1) {
+            if (prev is Para1 && next is Para1)
+            {
                 string num1 = prev.Number.Text.TrimStart('(').TrimEnd(')');
                 string num2 = next.Number.Text.TrimStart('(').TrimEnd(')');
                 if (num1.Length == 1 && num2.Length == 1)
@@ -71,12 +80,24 @@ namespace UK.Gov.Legislation.Lawmaker
             }
             // if (prev is Para2 !=  next is Para2)
             //     return false;
-            if (prev is Para2 && next is Para2) {
+            if (prev is Para2 && next is Para2)
+            {
                 string num1 = prev.Number.Text.TrimStart('(').TrimEnd(')');
                 string num2 = next.Number.Text.TrimStart('(').TrimEnd(')');
                 return Roman.LowerRomanToInt(num1) == Roman.LowerRomanToInt(num2) - 1;
             }
             return true;
+        }
+
+        private void AddTables(List<IBlock> container)
+        {
+            while (i < Document.Body.Count)
+            {
+                if (Document.Body[i].Block is not WTable table)
+                    break;
+                i += 1;
+                container.Add(table);
+            }
         }
 
     }
