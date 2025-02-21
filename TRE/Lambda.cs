eb0c5ac064de6d4d987a83927aaa7516fc73384f
mcpheed1 <dean.mcphee@au.leidos.com>
2025-02-21 12:18:03 +1100

    LNI-225: Created a distinct Lambda function handler for Lawmaker
    Co-authored-by: anthony-hashemi-tna-git <ant.hashemi@gmail.com>

--- TRE/Lambda.cs
+++ TRE/Lambda.cs
@@ -196,36 +196,4 @@ public class Lambda {
     }
 
 }
-
-public class Response {
-
-    [JsonPropertyName("parser-outputs")]
-    public ParserOutputs Outputs { get; set; }
-
-    public static Response Error(string error) => Response.Errors(new List<string>(1) { error });
-
-    public static Response Errors(IEnumerable<string> errors) =>
-        new Response { Outputs = new ParserOutputs { ErrorMessages = errors } };
-
-}
-
-public class ParserOutputs {
-
-    [JsonPropertyName("xml")]
-    public string XMLFilename { get; set; }
-
-    [JsonPropertyName("metadata")]
-    public string MetadataFilename { get; set; }
-
-    [JsonPropertyName("images")]
-    public IEnumerable<string> ImageFilenames { get; set; }
-
-    [JsonPropertyName("log")]
-    public string LogFilename { get; set; }
-
-    [JsonPropertyName("error-messages")]
-    public IEnumerable<string> ErrorMessages { get; set; }
-
-}
-
 }
