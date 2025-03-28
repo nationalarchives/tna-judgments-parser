43134bc8726ff449119d1d06d1d3dde3a70574d0
mcpheed1 <dean.mcphee@au.leidos.com>
2025-03-28 12:31:22 +1100

    LNI-226: Log maximum recursion depth of ParseAndMemoize method
    Co-authored-by: anthony-hashemi-tna-git <ant.hashemi@gmail.com>

--- src/lawmaker/parsers/HContainer.cs
+++ src/lawmaker/parsers/HContainer.cs
@@ -15,16 +15,25 @@ namespace UK.Gov.Legislation.Lawmaker
 
         // call only if line is Current()
         private T ParseAndMemoize<T>(WLine line, string name, System.Func<WLine, T> parseFunction) {
+            parseAndMemoizeDepth += 1;
+            if (parseAndMemoizeDepth > parseAndMemoizeDepthMax)
+                parseAndMemoizeDepthMax = parseAndMemoizeDepth;
             var key = (name, i, quoteDepth, frames.CurrentDocName, frames.CurrentContext);
             if (memo.TryGetValue(key, out var cached)) {
                 i = cached.NextPosition;
+                parseAndMemoizeDepth -= 1;
                 return (T)cached.Result;
             }
             int save = i;
+            parseDepth += 1;
+            if (parseDepth > parseDepthMax)
+                parseDepthMax = parseDepth;
             T result = parseFunction(line);
             if (result is null)
                 i = save;
             memo[key] = (result, i);
+            parseAndMemoizeDepth -= 1;
+            parseDepth -= 1;
             return result;
         }
 
@@ -290,7 +299,6 @@ namespace UK.Gov.Legislation.Lawmaker
                 return true;
             if (PeekScheduleCrossHeading(line))
                 return true;
-            // Todo: Add other grouping provisions?
             return false;
         }
         
