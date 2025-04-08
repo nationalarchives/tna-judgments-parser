5ca8c6d2bf7d5a1f160ec80d4475ff8816c6ada6
Jim Mangiafico <jim@jurisdatum.com>
2025-04-08 12:56:14 +0000

    keep track of uploaded files

--- backlog/src/Program.cs
+++ backlog/src/Program.cs
@@ -1,4 +1,9 @@
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Amazon.S3.Model;
+
 using Backlog.Src.Batch.One;
 
 namespace Backlog.Src
@@ -9,16 +14,39 @@ namespace Backlog.Src
 
         static int Main(string[] args)
         {
-            Batch.One.Helper helper = new() {
+            uint id = 58;
+            bool autoPublish = true;
+
+            DotNetEnv.Env.Load();  // required for bucket name
+
+            Helper helper = new()
+            {
                 PathToCourtMetadataFile = @"C:\Users\Administrator\TDR-2025-CNS6\court_metadata.csv",
                 PathDoDataFolder = @"C:\Users\Administrator\TDR-2025-CNS6\"
             };
-            // Files.CopyAllFilesWithExtension(helper.PathDoDataFolder, Metadata.Read(helper.PathToCourtMetadataFile));
+            Tracker tracker = new Tracker(@"C:\Users\Administrator\TDR-2025-CNS6\uploaded-staging.csv");
 
-            // Bundle bundle = helper.GenerateBundle(65);
+            List<Metadata.Line> lines = helper.FindLines(id);
 
-            // string output = @"C:\Users\Administrator\TDR-2025-CNS6\test.tar.gz";
-            // System.IO.File.WriteAllBytes(output, bundle.TarGz);
+            foreach (var line in lines)
+            {
+                if (tracker.WasDone(line))
+                    continue;
+
+                Bundle bundle = helper.GenerateBundle(line, autoPublish);
+
+                string output = @"C:\Users\Administrator\TDR-2025-CNS6\" + bundle.Uuid + ".tar.gz";
+                System.IO.File.WriteAllBytes(output, bundle.TarGz);
+
+                Task<PutObjectResponse> task = Bucket.UploadBundle(bundle.Uuid + ".tar.gz", bundle.TarGz);
+                var x = task.Result;
+
+                tracker.MarkDone(line, bundle.Uuid);
+
+                System.Console.WriteLine(bundle.Uuid + ".tar.gz");
+                System.Console.WriteLine(System.DateTime.Now);
+
+            }
 
             return 0;
         }
