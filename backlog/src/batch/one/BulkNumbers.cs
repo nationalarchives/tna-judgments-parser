4cf8a2b021d533eff98d0a81806c5135346a1c61
Anthony Hashemi <ant.hashemi@gmail.com>
2025-07-21 15:48:15 +0100

    Sepcify default of 0 for LAST_BEFORE_BATCH env var

--- backlog/src/batch/one/BulkNumbers.cs
+++ backlog/src/batch/one/BulkNumbers.cs
@@ -12,7 +12,7 @@ namespace Backlog.Src.Batch.One
     class BulkNumbers
     {
 
-        private static readonly uint LastBeforeThisBatch = uint.Parse(Environment.GetEnvironmentVariable("LAST_BEFORE_BATCH"));
+        private static readonly uint LastBeforeThisBatch = uint.Parse(Environment.GetEnvironmentVariable("LAST_BEFORE_BATCH") ?? "0");
 
         private static readonly string Path = Environment.GetEnvironmentVariable("BULK_NUMBERS_PATH") ?? System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bulk_numbers.csv");
 
