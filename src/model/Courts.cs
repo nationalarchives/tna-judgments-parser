dc666a776d5a5324a0c64e0e125928987e02f51f
Anthony Hashemi <ant.hashemi@gmail.com>
2025-08-06 16:19:16 +0100

    Add ConsumerCreditAppealsTribunal to Courts model

--- src/model/Courts.cs
+++ src/model/Courts.cs
@@ -396,6 +396,12 @@ public readonly partial struct Courts {
         CitationPattern = new Regex(@"^\[\d{4}\] UKIPTrib \d+$")
     };
 
+    public static readonly Court ConsumerCreditAppealsTribunal = new() {
+        Code = "UKFTT-Credit",
+        LongName = "Consumer Credit Appeals Tribunal",
+        URL = "https://webarchive.nationalarchives.gov.uk/ukgwa/20090516110219/http://www.consumercreditappeals.tribunals.gov.uk/",
+    };
+
     public static readonly Court[] All = {
         SupremeCourt,
         PrivyCouncil,
@@ -443,7 +449,8 @@ public readonly partial struct Courts {
 
         EmploymentTribunal,
 
-        InvestigatoryPowersTribunal
+        InvestigatoryPowersTribunal,
+        ConsumerCreditAppealsTribunal
     };
 
     public static readonly ImmutableDictionary<string, Court> ByCode =
