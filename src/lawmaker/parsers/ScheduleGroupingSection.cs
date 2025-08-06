bb81f61bd562be002f746c2f17cdabd50c023fe2
Francis Chataway <francis.chataway@au.leidos.com>
2025-08-06 22:27:02 +1000

    Pass extra document info for secondary types
    To support parsing UK secondary legislation, extra information needs to
    be present to reduce ambiguity when parsing. This commit extends the
    inputs from both the Lawmaker Lambda and the cli:
    - `"bill"` is not longer recognised as a hint, instead the doc name needs
      to be passed in, e.g. "nipubb", "uksi", etc.
    - `subtype` and "procedure" are now input options.Typed as strings until
      we port the enums from Lawmaker over
    This commit also:
    - Extends the `DocName` enum with draft legislation types as well as
      relevant mappings.
    - Renames the `BillParser` to the more generic `LegislationParser`

--- src/lawmaker/parsers/ScheduleGroupingSection.cs
+++ src/lawmaker/parsers/ScheduleGroupingSection.cs
@@ -6,7 +6,7 @@ using UK.Gov.Legislation.Judgments.Parse;
 namespace UK.Gov.Legislation.Lawmaker
 {
 
-    public partial class BillParser
+    public partial class LegislationParser
     {
 
         private HContainer ParseScheduleGroupingSection(WLine line)
