8b00316642f40b255151d17b621264440a66ece8
mcpheed1 <dean.mcphee@au.leidos.com>
2025-08-13 13:08:47 +1000

    LNI-279: Created an abstract LineEnricher class to allow for the creation of new enrichers without significant code duplication. Adjusted the existing QuotedText enricher accordingly.

--- src/lawmaker/enrichers/QuotedText.cs
+++ src/lawmaker/enrichers/QuotedText.cs
@@ -9,78 +9,16 @@ using DocumentFormat.OpenXml.Wordprocessing;
 namespace UK.Gov.Legislation.Lawmaker
 {
 
-    class QuotedTextEnricher
+    class QuotedTextEnricher : LineEnricher
     {
         private string Pattern;
-
-        internal delegate IInline Constructor(Match match, RunProperties props);
 
         public QuotedTextEnricher(string startPattern, string endPattern)
         {
             Pattern = @$"{startPattern}(?'contents'.+?)(?:{endPattern}|$)";
         }
 
-        internal void EnrichDivisions(IList<IDivision> divisions)
-        {
-            foreach (var div in divisions)
-                EnrichDivision(div);
-        }
-
-        internal void EnrichDivision(IDivision division)
-        {
-            if (division is Leaf leaf)
-                EnrichLeaf(leaf);
-            else if (division is Branch branch)
-                EnrichBranch(branch);
-        }
-
-        internal void EnrichBranch(Branch branch)
-        {
-            if (branch.Intro != null)
-                EnrichBlocks(branch.Intro);
-            EnrichDivisions(branch.Children);
-            if (branch.WrapUp != null)
-                EnrichBlocks(branch.WrapUp);
-        }
-
-        internal void EnrichLeaf(Leaf leaf)
-        {
-            if (leaf.Contents?.Count > 0)
-                EnrichBlocks(leaf.Contents);
-        }
-
-
-        internal void EnrichBlocks(IList<IBlock> blocks)
-        {
-            QuotedText qt = null;
-            IInline constructor(Match match, RunProperties props)
-            {
-                string startQuote = match.Groups["startQuote"].Value;
-                string contents = match.Groups["contents"].Value;
-                string endQuote = match.Groups["endQuote"].Value;
-                qt = new QuotedText() { Contents = [new WText(contents, props)], StartQuote = startQuote, EndQuote = endQuote };
-                return qt;
-            }
-
-            for (int i = 0; i < blocks.Count; i++)
-            {
-                IBlock enriched;
-                if (blocks[i] is WLine line && !line.NormalizedContent.StartsWith('\u201C'))
-                    enriched = EnrichLine(line, constructor);
-                else if (blocks[i] is Mod mod)
-                    enriched = EnrichMod(mod, constructor);
-                else
-                    continue;
-
-                if (qt is null)  // should be impossible
-                    continue;
-                blocks.RemoveAt(i);
-                blocks.Insert(i, enriched);
-                break;
-            }
-        }
-
-        internal Mod EnrichMod(Mod raw, Constructor constructor)
+        internal override Mod EnrichMod(Mod raw)
         {
             List<IBlock> enrichedBlocks = [];
             for (int i = 0; i < raw.Contents.Count; i++)
@@ -90,7 +28,7 @@ namespace UK.Gov.Legislation.Lawmaker
                  // mod elements to be created.
                 if (raw.Contents[i] is WLine line)
                 {
-                    WLine enrichedLine = Enrich(line, constructor);
+                    WLine enrichedLine = Enrich(line);
                     enrichedBlocks.Add(enrichedLine);
                 }
                 // Must enrich the divisions inside quoted structures
@@ -111,9 +49,12 @@ namespace UK.Gov.Legislation.Lawmaker
          * A wrapper for the 'Enrich' method that wraps the given line in a 
          * Mod element if it contains any QuotedText elements. 
          */
-        internal IBlock EnrichLine(WLine raw, Constructor constructor)
+        internal override IBlock EnrichLine(WLine raw)
         {
-            WLine enriched = Enrich(raw, constructor);
+            if (raw.NormalizedContent.StartsWith('\u201C'))
+                return raw;
+
+            WLine enriched = Enrich(raw);
             if (!ReferenceEquals(enriched, raw))
                 return new Mod() { Contents = [enriched] };
             return raw;
@@ -122,7 +63,7 @@ namespace UK.Gov.Legislation.Lawmaker
         /*
          * Identifies and extracts any QuotedText instances from the plaintext of a given line. 
          */
-        internal WLine Enrich(WLine raw, Constructor constructor)
+        internal WLine Enrich(WLine raw)
         {
             if (raw.Contents.Count() == 0 || raw.Contents.Last() is not WText current)
                 return raw;
@@ -138,11 +79,19 @@ namespace UK.Gov.Legislation.Lawmaker
                 if (match.Index > charIndex)
                     enrichedInlines.Add(new WText(text[charIndex..match.Index], current.properties));
                 charIndex = match.Index + match.Length;
-                enrichedInlines.Add(constructor(match, current.properties));
+                enrichedInlines.Add(constructQuotedText(match, current.properties));
             }
             if (charIndex != text.Length)
                 enrichedInlines.Add(new WText(text[charIndex..], current.properties));
             return WLine.Make(raw, enrichedInlines);
+        }
+
+        IInline constructQuotedText(Match match, RunProperties props)
+        {
+            string startQuote = match.Groups["startQuote"].Value;
+            string contents = match.Groups["contents"].Value;
+            string endQuote = match.Groups["endQuote"].Value;
+            return new QuotedText() { Contents = [new WText(contents, props)], StartQuote = startQuote, EndQuote = endQuote };
         }
 
     }
