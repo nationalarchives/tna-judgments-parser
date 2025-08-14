7ebef7e1c9fa45b797b3f80abad9658fb13c0c54
Francis Chataway <francis.chataway@au.leidos.com>
2025-08-14 19:24:38 +1000

    LNI-283: add support for table numbers and caption
    Lawmaker tables can be preceded by a table numer "Table X" and any
    number of captions or paragraphs between that number and the table
    itself. This commit adds support for parsing and building of those
    tables.
    
    This commit also makes some experimental changes by using a different
    style of parsing using methods like `Advance` and `Expect` rather than
    manipulating the raw `i` variable.
    
    Some potentially breaking changes include:
    - adding a `Block` virtual function to `Akn.Builder` to enable adding to
      the list of handled blocks in Lawmaker namespace.
    - some of the table building methods have been made static. I'd like to
      eventually build the table completely on it's own using `XElement`,
      making some of the methods static is a step towards that although
      there is still more to be done.
    
    Co-authored-by: Francis Chataway <francis.chataway@au.leidos.com>

--- src/lawmaker/parsers/LegislationParser.cs
+++ src/lawmaker/parsers/LegislationParser.cs
@@ -7,80 +7,76 @@ using AkN = UK.Gov.Legislation.Judgments.AkomaNtoso;
 using CaseLaw = UK.Gov.NationalArchives.CaseLaw.Parse;
 using DOCX = UK.Gov.Legislation.Judgments.DOCX;
 
-namespace UK.Gov.Legislation.Lawmaker
+namespace UK.Gov.Legislation.Lawmaker;
+public partial class LegislationParser
 {
 
-    public partial class LegislationParser
+    // We may need to hold this information in the Frames, but that may be tricky. For now we store them
+    // at the root.
+    private readonly string? subType;
+    private readonly string? procedure;
+    private readonly DocName docName;
+    /*
+        This class takes a list of "pre-parsed" blocks, and arranges them into a bill structure.
+        The pre-parsed list contains blocks of only four types:
+        - WLine (a line of text, corresponding to a Word "paragraph") (won't have a number unless subclassed)
+        - WOldNumberedParagraph (a subclass of WLine, with a number)
+        = WTable
+        - WTableOfContents
+    */
+    public static Document Parse(byte[] docx, LegislationClassifier classifier)
     {
-
-        // We may need to hold this information in the Frames, but that may be tricky. For now we store them
-        // at the root.
-        private readonly string? subType;
-        private readonly string? procedure;
-        private readonly DocName docName;
-        /*
-          This class takes a list of "pre-parsed" blocks, and arranges them into a bill structure.
-          The pre-parsed list contains blocks of only four types:
-           - WLine (a line of text, corresponding to a Word "paragraph") (won't have a number unless subclassed)
-           - WOldNumberedParagraph (a subclass of WLine, with a number)
-           = WTable
-           - WTableOfContents
-        */
-        public static Document Parse(byte[] docx, LegislationClassifier classifier)
-        {
-            WordprocessingDocument doc = AkN.Parser.Read(docx);
-            CaseLaw.WordDocument simple = new CaseLaw.PreParser().Parse(doc);
-            return new LegislationParser(simple, classifier).Parse();
-        }
+        WordprocessingDocument doc = AkN.Parser.Read(docx);
+        CaseLaw.WordDocument simple = new CaseLaw.PreParser().Parse(doc);
+        return new LegislationParser(simple, classifier).Parse();
+    }
 
         private LegislationParser(CaseLaw.WordDocument doc, LegislationClassifier classifier)
         {
             Document = doc;
             docName = classifier.DocName;
-            frames = new Frames(classifier.DocName, classifier.GetContext()); 
+            frames = new Frames(classifier.DocName, classifier.GetContext());
         }
 
-        private readonly ILogger Logger = Logging.Factory.CreateLogger<LegislationParser>();
-        private Frames frames;
-        private readonly CaseLaw.WordDocument Document;
-        private int i = 0;
+    private readonly ILogger Logger = Logging.Factory.CreateLogger<LegislationParser>();
+    private Frames frames;
+    private readonly CaseLaw.WordDocument Document;
+    private int i = 0;
 
-        int parseDepth = 0;
-        int parseDepthMax = 0;
-        int parseAndMemoizeDepth = 0;
-        int parseAndMemoizeDepthMax = 0;
+    int parseDepth = 0;
+    int parseDepthMax = 0;
+    int parseAndMemoizeDepth = 0;
+    int parseAndMemoizeDepthMax = 0;
 
-        private Lawmaker.Document Parse()
+    private Lawmaker.Document Parse()
+    {
+        ParseAndEnrichHeader();
+        ParseBody();
+
+        if (i != Document.Body.Count)
+            Logger.LogWarning("parsing did not complete: {}", i);
+
+        Logger.LogInformation($"Maximum ParseAndMemoize depth reached: {parseAndMemoizeDepthMax}");
+        Logger.LogInformation($"Maximum Parse depth reached: {parseDepthMax}");
+
+        // Handle start and end quotes after parsing is complete, because it alters the
+        // contents of parsed results which does not work well with memoization
+        ExtractAllQuotesAndAppendTexts(body);
+        QuotedTextEnricher quotedTextEnricher = new($"(?:{{.*?}})?{StartQuotePattern()}", EndQuotePattern());
+        quotedTextEnricher.EnrichDivisions(body);
+
+        var styles = DOCX.CSS.Extract(Document.Docx.MainDocumentPart, "#bill");
+
+        return new Lawmaker.Document
         {
-            ParseAndEnrichHeader();
-            ParseBody();
-
-            if (i != Document.Body.Count)
-                Logger.LogWarning("parsing did not complete: {}", i);
-
-            Logger.LogInformation($"Maximum ParseAndMemoize depth reached: {parseAndMemoizeDepthMax}");
-            Logger.LogInformation($"Maximum Parse depth reached: {parseDepthMax}");
-
-            // Handle start and end quotes after parsing is complete, because it alters the
-            // contents of parsed results which does not work well with memoization
-            ExtractAllQuotesAndAppendTexts(body);
-            QuotedTextEnricher quotedTextEnricher = new($"(?:{{.*?}})?{StartQuotePattern()}", EndQuotePattern());
-            quotedTextEnricher.EnrichDivisions(body);
-
-            var styles = DOCX.CSS.Extract(Document.Docx.MainDocumentPart, "#bill");
-
-            return new Lawmaker.Document
-            {
-                Type = docName,
-                Styles = styles,
-                CoverPage = coverPage,
-                Preface = preface,
-                Preamble = preamble,
-                Body = body,
-                Schedules = []
-            };
-        }
-
+            Type = docName,
+            Styles = styles,
+            CoverPage = coverPage,
+            Preface = preface,
+            Preamble = preamble,
+            Body = body,
+            Schedules = []
+        };
     }
 
 }
