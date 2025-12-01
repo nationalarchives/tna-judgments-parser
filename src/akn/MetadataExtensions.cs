81304328a965b575e429eee03479d7492543884a
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-12-01 18:32:30 +0000

    refactor: Make `MetadataExtensions` into extension methods

--- src/akn/MetadataExtensions.cs
+++ src/akn/MetadataExtensions.cs
@@ -7,59 +7,47 @@ using UK.Gov.NationalArchives.CaseLaw.Model;
 
 namespace judgments.src.akn;
 
-internal class MetadataExtensions
+internal static class MetadataExtensions
 {
-    internal static void AddProprietaryFields(XmlElement proprietary, IMetadataExtended meta)
+    internal static void AddProprietaryFields(this XmlElement proprietary, IMetadataExtended meta)
     {
-        if (meta.Parties is not null)
+        foreach (var party in meta.Parties)
         {
-            foreach (var party in meta.Parties)
-            {
-                if (party is not null)
-                {
-                    AddParty(proprietary, party);
-                }
-            }
+            proprietary.AddParty(party);
         }
 
-        if (meta.Categories is not null)
+        foreach (var cat in meta.Categories)
         {
-            foreach (var cat in meta.Categories)
-            {
-                if (cat is not null)
-                {
-                    AddCategory(proprietary, cat);
-                }
-            }
+            proprietary.AddCategory(cat);
         }
 
         if (meta.SourceFormat is not null)
         {
-            AddProprietaryField(proprietary, "sourceFormat", meta.SourceFormat);
+            proprietary.AddProprietaryField("sourceFormat", meta.SourceFormat);
         }
 
         if (meta.WebArchivingLink is not null)
         {
-            AddProprietaryField(proprietary, "webarchiving", meta.WebArchivingLink);
+            proprietary.AddProprietaryField("webarchiving", meta.WebArchivingLink);
         }
     }
 
-    private static void AddParty(XmlElement proprietary, Party party)
+    private static void AddParty(this XmlElement proprietary, Party party)
     {
-        var e = AddProprietaryField(proprietary, "party", party.Name);
+        var e = proprietary.AddProprietaryField("party", party.Name);
         e.SetAttribute("role", party.Role.ShowAs());
     }
 
-    private static void AddCategory(XmlElement proprietary, ICategory cat)
+    private static void AddCategory(this XmlElement proprietary, ICategory cat)
     {
-        var e = AddProprietaryField(proprietary, "category", cat.Name);
+        var e = proprietary.AddProprietaryField("category", cat.Name);
         if (cat.Parent is not null)
         {
             e.SetAttribute("parent", cat.Parent);
         }
     }
 
-    internal static XmlElement AddProprietaryField(XmlElement proprietary, string name, string value)
+    internal static XmlElement AddProprietaryField(this XmlElement proprietary, string name, string value)
     {
         var e = proprietary.OwnerDocument.CreateElement("uk", name, Metadata.ukns);
         proprietary.AppendChild(e);
