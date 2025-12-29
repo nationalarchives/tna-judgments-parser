2d95561ac46ec619a41d820f4008129caf68a6de
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-12-29 13:33:16 +0000

    feat(backlog): Add file logger and improve log outputs

--- backlog/src/Program.cs
+++ backlog/src/Program.cs
@@ -15,7 +15,7 @@ using UK.Gov.NationalArchives.Judgments.Api;
 
 namespace Backlog.Src;
 
-public static class Program
+public class Program
 {
     private static readonly Option<bool> DryRunOption = new("--dry-run")
     {
@@ -83,8 +83,17 @@ public static class Program
         var trackerPath = Environment.GetEnvironmentVariable("TRACKER_PATH") ?? Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "uploaded-production.csv");
         var tracker = new Tracker(trackerPath);
 
-        Log($"Using data folder: {pathToDataFolder}");
-        Log($"Using court metadata from: {pathToCourtMetadataFile}");
+        var logFilePath = Path.Combine(pathToDataFolder, $"log_{DateTime.Now:yy-MM-dd_HH-mm}.txt");
+        var loggerFactory = LoggerFactory.Create(x =>
+            x.AddConsole()
+             .AddFile(logFilePath,
+                 outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] {Message}{NewLine}{Exception}"));
+        var logger = loggerFactory.CreateLogger<Program>();
+
+        logger.LogInformation("Using Parser version: {ParserVersion}",
+            UK.Gov.Legislation.Judgments.AkomaNtoso.Metadata.GetParserVersion());
+        logger.LogInformation("Using data folder: {PathToDataFolder}", pathToDataFolder);
+        logger.LogInformation("Using court metadata from: {PathToCourtMetadataFile}", pathToCourtMetadataFile);
 
         List<Metadata.Line> lines;
         if (id.HasValue)
@@ -93,7 +102,7 @@ public static class Program
             lines = helper.FindLines(id.Value);
             if (!lines.Any())
             {
-                Log($"No records found for id {id.Value}");
+                logger.LogCritical("No records found for id {SuppliedId}", id.Value);
                 return 1;
             }
         }
@@ -103,91 +112,119 @@ public static class Program
             lines = Metadata.Read(helper.PathToCourtMetadataFile);
             if (!lines.Any())
             {
-                Log("No records found in the metadata file");
+                logger.LogCritical("No records found in the metadata file");
                 return 1;
             }
         }
 
         var alreadyDoneLines = new List<Metadata.Line>();
-        var successfulLines = new List<Metadata.Line>();
+        var successfulNewLines = new List<Metadata.Line>();
         var failedLines = new List<(Metadata.Line line, Exception exception)>();
 
-        foreach (var line in lines)
+        for (var i = 0; i < lines.Count; i++)
         {
-            if (tracker.WasDone(line))
-            {
-                Log("skipping " + line.id);
-                alreadyDoneLines.Add(line);
-                continue;
-            }
+            var line = lines[i];
 
             try
             {
-                Log($"Processing file: {line.FilePath}");
+                if (tracker.WasDone(line))
+                {
+                    logger.LogWarning("skipping {LineId}", line.id);
+                    alreadyDoneLines.Add(line);
+                    continue;
+                }
+
+                logger.LogInformation("Processing file: {FilePath}", line.FilePath);
                 var bundle = helper.GenerateBundle(line, judgmentsFilePath, hmctsFilePath, autoPublish);
 
-                var output = Path.Combine(pathToOutputFolder, bundle.Uuid + ".tar.gz");
-                Log($"  Writing to output: {output}");
+                var bundleFileName = bundle.Uuid + ".tar.gz";
+                var output = Path.Combine(pathToOutputFolder, bundleFileName);
+                logger.LogInformation("  Writing to output: {Output}", output);
                 File.WriteAllBytes(output, bundle.TarGz);
-
 
                 if (isDryRun)
                 {
-                    Log("  This is a dry run - not uploading to S3");
+                    logger.LogInformation("  This is a dry run - not uploading to S3");
                 }
                 else
                 {
-                    Log("  Uploading to S3");
-                    Bucket.UploadBundle(bundle.Uuid + ".tar.gz", bundle.TarGz).Wait();
+                    logger.LogInformation("  Uploading {BundleFileName} to S3", bundleFileName);
+                    Bucket.UploadBundle(bundleFileName, bundle.TarGz).Wait();
                 }
 
                 tracker.MarkDone(line, bundle.Uuid);
-                successfulLines.Add(line);
+                successfulNewLines.Add(line);
 
-                Log("  success");
+                logger.LogInformation("  success");
             }
             catch (Exception ex)
             {
-                Log($"Error processing line {line.id}:");
-                Log(ex.ToString());
+                logger.LogError(ex, "Error processing line {LineId}:", line.id);
                 failedLines.Add((line, ex));
+            }
+            finally
+            {
+                logger.LogInformation("{Percent}% done", 100 * (i + 1) / lines.Count);
             }
         }
 
-        Log("---------------------------", false);
-
-        Log($"Processed {alreadyDoneLines.Count + successfulLines.Count + failedLines.Count} of {lines.Count} lines");
-        Log($"Successfully processed {successfulLines.Count} lines");
-        Log($"Skipped {alreadyDoneLines.Count} lines that had previously been processed");
+        LogFinalStatistics(logger, alreadyDoneLines, successfulNewLines, lines, failedLines);
 
         if (failedLines.Count > 0)
         {
-            Log($"Failed to process {failedLines.Count} lines");
-
-            var failedIdsGroupedByErrorMessage = failedLines.GroupBy(f => f.exception.Message, f => f.line.id);
-            foreach (var thing in failedIdsGroupedByErrorMessage)
-            {
-                Log($"  {thing.Count()} lines failed with exception message {thing.Key}");
-                Log($"    Ids affected were: ({string.Join(" ,", thing)})");
-            }
-
             return 1;
         }
-
-        Log("No failed lines");
 
         return 0;
     }
 
-    private static void Log(string message, bool includeTimestamp = true)
+    private static void LogFinalStatistics(ILogger logger, List<Metadata.Line> alreadyDoneLines,
+        List<Metadata.Line> successfulNewLines, List<Metadata.Line> lines,
+        List<(Metadata.Line line, Exception exception)> failedLines)
     {
-        if (includeTimestamp)
+        logger.LogInformation("""
+                              ---------------------------
+                              Successfully processed {SuccessfulLinesCount} of {CsvLinesCount} parsed csv lines, of which:
+                                - {NewLinesCount} lines were new
+                                - {AlreadyDoneLineCount} lines were skipped (because they had been processed in a previous run)
+                              """,
+            alreadyDoneLines.Count + successfulNewLines.Count,
+            lines.Count,
+            successfulNewLines.Count,
+            alreadyDoneLines.Count
+        );
+
+        if (failedLines.Count > 0)
         {
-            Console.WriteLine("{0:G}: {1}", DateTime.Now, message);
+            logger.LogError("Failed to process {FailedLineCount} lines, of which:", failedLines.Count);
+
+            var failedIdsGroupedByErrorMessage = failedLines
+                .GroupBy(f =>
+                    {
+                        return f.exception.Message switch
+                        {
+                            _ when f.exception.Message.StartsWith("Could not find file") => "Could not find file",
+                            _ when f.exception.Message.StartsWith("Couldn't find file with UUID") =>
+                                "Couldn't find file with UUID",
+                            _ when f.exception.Message.EndsWith("was not recognized as a valid DateTime.") =>
+                                "String was not recognized as a valid DateTime",
+                            _ => f.exception.Message
+                        };
+                    },
+                    f => f.line.id);
+            foreach (var groupOfErrors in failedIdsGroupedByErrorMessage)
+            {
+                var affectedIds = groupOfErrors.Count() <= 5
+                    ? string.Join(", ", groupOfErrors)
+                    : string.Join(", ", groupOfErrors.Take(5)) + "...";
+                logger.LogError(
+                    "  - {Count} lines failed with exception message \"{Message}\". Ids affected were: ({AffectedIds})",
+                    groupOfErrors.Count(), groupOfErrors.Key, affectedIds);
+            }
         }
         else
         {
-            Console.WriteLine(message);
+            logger.LogInformation("No failed lines");
         }
     }
 }
