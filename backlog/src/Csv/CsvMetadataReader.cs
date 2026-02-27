1231d371fc785700429b4ffbb0f2df5a75bd8d94
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2026-02-27 14:45:58 +0000

    fix(backlog): fix nullable warning when populating FullCsvLineContents

--- backlog/src/Csv/CsvMetadataReader.cs
+++ backlog/src/Csv/CsvMetadataReader.cs
@@ -123,7 +123,7 @@ class CsvMetadataReader(ILogger<CsvMetadataReader> logger)
                 {
                     var headerNames = convertFromStringArgs.Row.HeaderRecord!;
                     return headerNames.ToDictionary(headerName => headerName.Trim(),
-                        headerName => convertFromStringArgs.Row[headerName]);
+                                          headerName => convertFromStringArgs.Row[headerName] ?? string.Empty);
                 });
         }
     }
