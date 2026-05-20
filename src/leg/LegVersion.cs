4f3619fce15265a57250a8c3c2da9fff1a5b82dc
QR-TNA <quddus.rahman@nationalarchives.gov.uk>
2026-05-20 12:26:36 +0100

    docs(leg): document LegVersion bump policy
    SemVer scoped to AKN output impact (MAJOR = breaking contract change,
    MINOR = new doc-type / new emission, PATCH = output-changing bug fix).
    PR author bumps in the same PR when leg behaviour changes; byte-identical
    refactors don't bump. LegVersion.cs docstring points to the new policy doc.

--- src/leg/LegVersion.cs
+++ src/leg/LegVersion.cs
@@ -2,8 +2,9 @@ namespace UK.Gov.Legislation {
 
 /// <summary>
 /// Version of the leg parser, emitted as the <c>Name="legislation"</c>
-/// entry of <c>&lt;ukm:Parser&gt;</c>. Bump on leg-side behaviour
-/// changes that readers of the AKN should notice.
+/// entry of <c>&lt;ukm:Parser&gt;</c>. See <c>src/leg/VERSIONING.md</c>
+/// for the bump policy (SemVer scoped to AKN output impact; bumped by
+/// the PR author when leg behaviour changes).
 /// </summary>
 internal static class LegVersion {
 
