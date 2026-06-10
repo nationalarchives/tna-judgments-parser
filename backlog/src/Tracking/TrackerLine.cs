f0a5db6e56ee6e91f83e326c626deec4c16fc69f
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2026-06-10 15:25:43 +0100

    refactor(backlog): Remove unused csvline from TrackerLine

--- backlog/src/Tracking/TrackerLine.cs
+++ backlog/src/Tracking/TrackerLine.cs
@@ -2,23 +2,15 @@
 
 using System;
 
-using Backlog.Csv;
-
 using CsvHelper.Configuration.Attributes;
 
 namespace Backlog.Tracking;
 
 internal class TrackerLine
 {
-    public string? Court { get; set;}
-    public string? FileExtension {get; set; }
-    
-    
+    public string? Court { get; set; }
+    public string? FileExtension { get; set; }
     public Guid SourceUuid { get; init; }
-
-    [Ignore]
-    public CsvLine? CsvLine { get; init; }
-
     public Guid ParserRunId { get; set; }
     public TrackerStatus TrackerStatus { get; set; }
     public string? TreReference { get; set; }
@@ -31,6 +23,4 @@ internal class TrackerLine
 
     [Format("yyyy-MM-dd HH:mm:ss.fff")]
     public DateTimeOffset TrackerLineLastUpdated { get; set; }
-
-    
 }
