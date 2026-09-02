d2f8b6ddae4341061621a3e4ed9fa9d6230e898c
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2026-09-02 13:20:38 +0100

    refactor(TestNoChange): lint and small tidy for readability

--- test/TRE/TestNoChange.cs
+++ test/TRE/TestNoChange.cs
@@ -1,4 +1,8 @@
+#nullable enable
+
 using System.Linq;
+
+using Shouldly;
 
 using test.ApiTests;
 
@@ -8,58 +12,59 @@ using Xunit;
 
 using Api = UK.Gov.NationalArchives.Judgments.Api;
 
-namespace test.TRE
+namespace test.TRE;
+
+public class TestNoChange
 {
-    public class TestNoChange
+    private static readonly int[] FailingTestsToSkip = [6, 8, 31, 32];
+    public static readonly TheoryData<int> PassingTests = new(Tests.Indices.Except(FailingTestsToSkip));
+
+    [Theory]
+    [MemberData(nameof(PassingTests))]
+    public void TestJudgments_WithMetadataFromOutputOfCleanRun_AreTheSameAsOutputFromCleanRun(int i)
     {
-        private static readonly int[] FailingTestsToSkip = [6, 8, 31, 32];
-        public static readonly TheoryData<int> PassingTests = new(Tests.Indices.Except(FailingTestsToSkip));
+        var docx = DocumentHelpers.ReadDocx(i);
+        var cleanRunResponse = TestInputInjection.LambdaTest(docx, null);
 
-        [Theory]
-        [MemberData(nameof(PassingTests))]
-        public void TestJudgments_WithMetadataFromOutputOfCleanRun_AreTheSameAsOutputFromCleanRun(int i)
+        var externalMetadataFromCleanRun = new ParserInputs
         {
-            byte[] docx = DocumentHelpers.ReadDocx(i);
-            Api.Response response1 = TestInputInjection.LambdaTest(docx, null);
-            ParserInputs inputs = new()
+            DocumentType = cleanRunResponse.Meta.DocumentType,
+            Metadata = new InputMetadata
             {
-                DocumentType = response1.Meta.DocumentType,
-                Metadata = new InputMetadata()
-                {
-                    URI = Api.URI.ExtractShortURIComponent(response1.Meta.Uri),
-                    Cite = response1.Meta.Cite,
-                    Court = response1.Meta.Court,
-                    Date = response1.Meta.Date,
-                    Name = response1.Meta.Name
-                }
-            };
-            Api.Response response2 = TestInputInjection.LambdaTest(docx, inputs);
-            Assert.Equal(response1.Meta.Uri, response2.Meta.Uri);
-            Assert.Equal(response1.Meta.Cite, response2.Meta.Cite);
-            Assert.Equal(response1.Meta.Court, response2.Meta.Court);
-            Assert.Equal(response1.Meta.Date, response2.Meta.Date);
-            Assert.Equal(response1.Meta.Name, response2.Meta.Name);
-            string actual = DocumentHelpers.RemoveNonDeterministicMetadata(response1.Xml, Xslt);
-            string expected = DocumentHelpers.RemoveNonDeterministicMetadata(response2.Xml, Xslt);
-            Assert.Equal(expected, actual);
-        }
+                URI = Api.URI.ExtractShortURIComponent(cleanRunResponse.Meta.Uri),
+                Cite = cleanRunResponse.Meta.Cite,
+                Court = cleanRunResponse.Meta.Court,
+                Date = cleanRunResponse.Meta.Date,
+                Name = cleanRunResponse.Meta.Name
+            }
+        };
 
-        /// <summary>
-        /// if the parser is given a date, it can't tell whether it's a 'decision' or a 'hearing' date
-        /// </summary>
-        private const string Xslt = @"<?xml version='1.0'?>
-<xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform' version='1.0' xmlns:akn='http://docs.oasis-open.org/legaldocml/ns/akn/3.0' xmlns:uk='https://caselaw.nationalarchives.gov.uk/akn'>
-  <xsl:template match='akn:FRBRManifestation/akn:FRBRdate/@date'/>
-  <xsl:template match=""akn:FRBRdate/@name[.='hearing']"">
-    <xsl:attribute name=""name"">decision</xsl:attribute>
-  </xsl:template>
-  <xsl:template match='@*|node()'>
-    <xsl:copy>
-      <xsl:apply-templates select='@*|node()'/>
-    </xsl:copy>
-  </xsl:template>
-</xsl:stylesheet>";
+        var runWithExternalMetadataResponse = TestInputInjection.LambdaTest(docx, externalMetadataFromCleanRun);
 
+        runWithExternalMetadataResponse.Meta.ShouldBeEquivalentTo(cleanRunResponse.Meta);
+
+        var cleanRunResponseXml = DocumentHelpers.RemoveNonDeterministicMetadata(cleanRunResponse.Xml, Xslt);
+        var runWithExternalMetadataResponseXml =
+            DocumentHelpers.RemoveNonDeterministicMetadata(runWithExternalMetadataResponse.Xml, Xslt);
+
+        Assert.Equal(cleanRunResponseXml, runWithExternalMetadataResponseXml);
     }
 
+    /// <summary>
+    /// if the parser is given a date, it can't tell whether it's a 'decision' or a 'hearing' date
+    /// </summary>
+    private const string Xslt = """
+                                <?xml version='1.0'?>
+                                <xsl:stylesheet xmlns:xsl='http://www.w3.org/1999/XSL/Transform' version='1.0' xmlns:akn='http://docs.oasis-open.org/legaldocml/ns/akn/3.0' xmlns:uk='https://caselaw.nationalarchives.gov.uk/akn'>
+                                  <xsl:template match='akn:FRBRManifestation/akn:FRBRdate/@date'/>
+                                  <xsl:template match="akn:FRBRdate/@name[.='hearing']">
+                                    <xsl:attribute name="name">decision</xsl:attribute>
+                                  </xsl:template>
+                                  <xsl:template match='@*|node()'>
+                                    <xsl:copy>
+                                      <xsl:apply-templates select='@*|node()'/>
+                                    </xsl:copy>
+                                  </xsl:template>
+                                </xsl:stylesheet>
+                                """;
 }
