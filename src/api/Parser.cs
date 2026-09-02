1bb64eca7173fa3f63355ca019b0f055118a35d2
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2026-09-02 18:09:52 +0100

    lint(parser): run linter and fix minor sonar warnings

--- src/api/Parser.cs
+++ src/api/Parser.cs
@@ -1,35 +1,31 @@
-
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Xml;
 using System.Xml.Schema;
-
-using DocumentFormat.OpenXml.Packaging;
 
 using Microsoft.Extensions.Logging;
 
 using UK.Gov.Legislation.Judgments;
-using UK.Gov.NationalArchives.CaseLaw.Parse;
-using AkN = UK.Gov.Legislation.Judgments.AkomaNtoso;
-using PS = UK.Gov.NationalArchives.CaseLaw.PressSummaries;
-
-using AttachmentPair = System.Tuple<byte[], UK.Gov.Legislation.Judgments.AttachmentType>;
-using ParseFunction = System.Func<byte[], UK.Gov.Legislation.Judgments.IOutsideMetadata, System.Collections.Generic.IEnumerable<System.Tuple<byte[], UK.Gov.Legislation.Judgments.AttachmentType>>, UK.Gov.Legislation.Judgments.AkomaNtoso.ILazyBundle>;
-
-using AttachmentPair1 = System.Tuple<DocumentFormat.OpenXml.Packaging.WordprocessingDocument, UK.Gov.Legislation.Judgments.AttachmentType>;
-using OptimizedParseFunction = System.Func<DocumentFormat.OpenXml.Packaging.WordprocessingDocument, UK.Gov.NationalArchives.CaseLaw.Parse.WordDocument, UK.Gov.Legislation.Judgments.IOutsideMetadata, System.Collections.Generic.IEnumerable<System.Tuple<DocumentFormat.OpenXml.Packaging.WordprocessingDocument, UK.Gov.Legislation.Judgments.AttachmentType>>, UK.Gov.Legislation.Judgments.Parse.Judgment>;
 using UK.Gov.Legislation.Judgments.Parse;
+using UK.Gov.NationalArchives.CaseLaw.Parse;
+
+using AkN = UK.Gov.Legislation.Judgments.AkomaNtoso;
+using AttachmentPair = System.Tuple<byte[], UK.Gov.Legislation.Judgments.AttachmentType>;
+using OptimizedParseFunction = System.Func<DocumentFormat.OpenXml.Packaging.WordprocessingDocument,
+    UK.Gov.NationalArchives.CaseLaw.Parse.WordDocument, UK.Gov.Legislation.Judgments.IOutsideMetadata, System.Collections.Generic.IEnumerable<System.Tuple<DocumentFormat.OpenXml.Packaging.WordprocessingDocument,
+        UK.Gov.Legislation.Judgments.AttachmentType>>, UK.Gov.Legislation.Judgments.Parse.Judgment>;
+using ParseFunction = System.Func<byte[], UK.Gov.Legislation.Judgments.IOutsideMetadata, System.Collections.Generic.IEnumerable<System.Tuple<byte[], UK.Gov.Legislation.Judgments.AttachmentType>>,
+    UK.Gov.Legislation.Judgments.AkomaNtoso.ILazyBundle>;
+using PS = UK.Gov.NationalArchives.CaseLaw.PressSummaries;
 
 namespace UK.Gov.NationalArchives.Judgments.Api;
 
 public enum Hint { UKSC, EWCA, EWHC, UKUT, Judgment, PressSummary }
 
-public class InvalidAkNException : Exception {
-
-    public InvalidAkNException(ValidationEventArgs cause) : base(cause.Message, cause.Exception) { }
-}
+public class InvalidAkNException(ValidationEventArgs cause) : Exception(cause.Message, cause.Exception);
 
 public interface IParser
 {
@@ -39,200 +35,274 @@ public interface IParser
 public class Parser(ILogger<Parser> logger, AkN.IValidator validator) : IParser
 {
     /// <exception cref="InvalidAkNException"></exception>
-    public Response Parse(Request request) {
-
+    public Response Parse(Request request)
+    {
         if (request.Filename is not null)
-            logger.LogInformation($"parsing { request.Filename }");
+        {
+            logger.LogInformation("parsing {RequestFilename}", request.Filename);
+        }
 
-        ParseFunction parse = GetParser(request.Hint);
+        var parse = GetParser(request.Hint);
 
-        IOutsideMetadata meta1 = (request.Meta is null) ? null : new MetaWrapper() { Meta = request.Meta };
-        IEnumerable<AttachmentPair> attachments = (request.Attachments is null) ? Enumerable.Empty<AttachmentPair>() : request.Attachments.Select(a => ConvertAttachment(a));
+        IOutsideMetadata meta1 = request.Meta is null ? null : new MetaWrapper { Meta = request.Meta };
+        var attachments = request.Attachments is null
+            ? Enumerable.Empty<AttachmentPair>()
+            : request.Attachments.Select(a => new AttachmentPair(a.Content, MapAttachmentType(a.Type)));
 
-        AkN.ILazyBundle bundle = parse(request.Content, meta1, attachments);
+        var bundle = parse(request.Content, meta1, attachments);
 
-        List<ValidationEventArgs> errors = validator.Validate(bundle.Judgment);
+        var errors = validator.Validate(bundle.Judgment);
         if (errors.Any())
+        {
             throw new InvalidAkNException(errors.First());
+        }
 
-        string xml = SerializeXml(bundle.Judgment);
-        AkN.Meta aknMetadata = AkN.MetadataExtractor.Extract(bundle.Judgment);
-        Meta meta2 = ConvertInternalMetadata(aknMetadata);
+        var xml = SerializeXml(bundle.Judgment);
+        var aknMetadata = AkN.MetadataExtractor.Extract(bundle.Judgment);
+        var meta2 = ConvertInternalMetadata(aknMetadata);
         Log(meta2);
-        List<Image> images = bundle.Images.Select(i => ConvertImage(i)).ToList();
+        var images = bundle.Images.Select(ConvertImage).ToList();
 
         bundle.Dispose();
 
-        return new Response() {
+        return new Response
+        {
             Xml = xml,
             Meta = meta2,
             Images = images
         };
     }
 
-    private ParseFunction GetParser(Hint? hint) {
-        if (!hint.HasValue)
-            return JudgmentOrPressSummary;
-        if (hint.Value == Hint.Judgment)
-            return ParseAnyJudgment;
-        if (hint.Value == Hint.EWHC || hint.Value == Hint.EWCA)
-            return Wrap(OptimizedEWHCParser.Parse);
-        if (hint.Value == Hint.UKSC)
-            return Wrap(OptimizedUKSCParser.Parse);
-        if (hint.Value == Hint.UKUT)
-            return Wrap(OptimizedUKUTParser.Parse);
-        if (hint.Value == Hint.PressSummary)
-            return ParsePressSummary;
-        throw new Exception("unsupported hint: " + Enum.GetName(typeof(Hint), hint));
+    private ParseFunction GetParser(Hint? hint)
+    {
+        return hint switch
+        {
+            null => JudgmentOrPressSummary,
+            Hint.Judgment => ParseAnyJudgment,
+            Hint.EWHC or Hint.EWCA => Wrap(OptimizedEWHCParser.Parse),
+            Hint.UKSC => Wrap(OptimizedUKSCParser.Parse),
+            Hint.UKUT => Wrap(OptimizedUKUTParser.Parse),
+            Hint.PressSummary => ParsePressSummary,
+            _ => throw new ArgumentOutOfRangeException(nameof(hint), hint, "unsupported hint")
+        };
     }
 
-    private static ParseFunction Wrap(OptimizedParseFunction f) {
-        return (docx, meta, attachments) => {
-            WordprocessingDocument doc = AkN.Parser.Read(docx);
-            WordDocument preParsed = new PreParser().Parse(doc);
-            IEnumerable<AttachmentPair1> attach2 = AkN.Parser.ConvertAttachments(attachments);
+    private static ParseFunction Wrap(OptimizedParseFunction f)
+    {
+        return (docx, meta, attachments) =>
+        {
+            var doc = AkN.Parser.Read(docx);
+            var preParsed = new PreParser().Parse(doc);
+            var attach2 = AkN.Parser.ConvertAttachments(attachments);
             IJudgment judgment = f(doc, preParsed, meta, attach2);
             return new AkN.Bundle(doc, judgment);
         };
     }
 
-    private AkN.ILazyBundle ParseAnyJudgment(byte[] docx, IOutsideMetadata meta, IEnumerable<System.Tuple<byte[], UK.Gov.Legislation.Judgments.AttachmentType>> attachments) {
-        WordprocessingDocument doc = AkN.Parser.Read(docx);
-        WordDocument preParsed = new PreParser().Parse(doc);
+    private AkN.ILazyBundle ParseAnyJudgment(byte[] docx, IOutsideMetadata meta,
+        IEnumerable<Tuple<byte[], Legislation.Judgments.AttachmentType>> attachments)
+    {
+        var doc = AkN.Parser.Read(docx);
+        var preParsed = new PreParser().Parse(doc);
         IJudgment judgment = BestJudgment(preParsed, meta, attachments);
         return new AkN.Bundle(doc, judgment);
     }
 
-    private Judgment BestJudgment(WordDocument preParsed, IOutsideMetadata meta, IEnumerable<System.Tuple<byte[], UK.Gov.Legislation.Judgments.AttachmentType>> attachments) {
-        IEnumerable<AttachmentPair1> attach2 = AkN.Parser.ConvertAttachments(attachments);
+    private Judgment BestJudgment(WordDocument preParsed, IOutsideMetadata meta,
+        IEnumerable<Tuple<byte[], Legislation.Judgments.AttachmentType>> attachments)
+    {
+        var attach2 = AkN.Parser.ConvertAttachments(attachments);
         OptimizedParseFunction first = OptimizedEWHCParser.Parse;
-        List<OptimizedParseFunction> others = new List<OptimizedParseFunction>(2) {
-            OptimizedUKSCParser.Parse,
-            OptimizedUKUTParser.Parse
-        };
-        Judgment judgment1 = first(preParsed.Docx, preParsed, meta, attach2);
-        int score1 = Score(judgment1);
+        var others = new List<OptimizedParseFunction>(2) { OptimizedUKSCParser.Parse, OptimizedUKUTParser.Parse };
+        var judgment1 = first(preParsed.Docx, preParsed, meta, attach2);
+        var score1 = Score(judgment1);
         if (score1 == PerfectScore)
+        {
             return judgment1;
-        foreach (var other in others) {
-            Judgment judgment2 = other(preParsed.Docx, preParsed, meta, attach2);
-            int score2 = Score(judgment2);
+        }
+
+        foreach (var other in others)
+        {
+            var judgment2 = other(preParsed.Docx, preParsed, meta, attach2);
+            var score2 = Score(judgment2);
             if (score2 == PerfectScore)
+            {
                 return judgment2;
-            if (score2 > score1) {
+            }
+
+            if (score2 > score1)
+            {
                 judgment1 = judgment2;
                 score1 = score2;
             }
         }
+
         return judgment1;
     }
 
-    private AkN.ILazyBundle JudgmentOrPressSummary(byte[] docx, IOutsideMetadata meta, IEnumerable<System.Tuple<byte[], UK.Gov.Legislation.Judgments.AttachmentType>> attachments) {
-        WordprocessingDocument doc = AkN.Parser.Read(docx);
-        WordDocument preParsed = new PreParser().Parse(doc);
+    private AkN.ILazyBundle JudgmentOrPressSummary(byte[] docx, IOutsideMetadata meta,
+        IEnumerable<Tuple<byte[], Legislation.Judgments.AttachmentType>> attachments)
+    {
+        var doc = AkN.Parser.Read(docx);
+        var preParsed = new PreParser().Parse(doc);
 
-        Judgment judgment = BestJudgment(preParsed, meta, attachments);
+        var judgment = BestJudgment(preParsed, meta, attachments);
         if (Score(judgment) == PerfectScore)
+        {
             return new AkN.Bundle(doc, judgment);
+        }
 
-        PS.PressSummary ps = PS.Parser.Parse(preParsed, meta);
+        var ps = PS.Parser.Parse(preParsed, meta);
         if (ps.InternalMetadata.DocType is not null)
+        {
             return new AkN.PSBundle(doc, ps);
+        }
 
         return new AkN.Bundle(doc, judgment);
     }
 
-    private static int PerfectScore = 7;
+    private static readonly int PerfectScore = 7;
 
-    private static int Score(Judgment judgment) {
-        int score = 0;
+    private static int Score(Judgment judgment)
+    {
+        var score = 0;
         if (judgment.Header is not null && judgment.Header.Any())
+        {
             score += 2;
+        }
+
         if (judgment.InternalMetadata.ShortUriComponent is not null)
+        {
             score += 1;
+        }
+
         if (judgment.InternalMetadata.Court is not null)
+        {
             score += 1;
+        }
+
         if (judgment.InternalMetadata.Cite is not null)
+        {
             score += 1;
+        }
+
         if (judgment.InternalMetadata.Date is not null)
+        {
             score += 1;
+        }
+
         if (judgment.InternalMetadata.Name is not null)
+        {
             score += 1;
+        }
+
         return score;
     }
 
-    private AkN.ILazyBundle ParsePressSummary(byte[] docx, IOutsideMetadata meta, IEnumerable<System.Tuple<byte[], UK.Gov.Legislation.Judgments.AttachmentType>> attachments) {
-        WordprocessingDocument doc = AkN.Parser.Read(docx);
-        PS.PressSummary ps = PS.Parser.Parse(doc, meta);
+    private AkN.ILazyBundle ParsePressSummary(byte[] docx, IOutsideMetadata meta,
+        IEnumerable<Tuple<byte[], Legislation.Judgments.AttachmentType>> attachments)
+    {
+        var doc = AkN.Parser.Read(docx);
+        var ps = PS.Parser.Parse(doc, meta);
         return new AkN.PSBundle(doc, ps);
     }
 
-    /* */
-
-    internal static string SerializeXml(XmlDocument judgment) {
-        using MemoryStream memStrm = new MemoryStream();
+    internal static string SerializeXml(XmlDocument judgment)
+    {
+        using var memStrm = new MemoryStream();
         AkN.Serializer.Serialize(judgment, memStrm);
-        return System.Text.Encoding.UTF8.GetString(memStrm.ToArray());
+        return Encoding.UTF8.GetString(memStrm.ToArray());
     }
 
-    internal static Meta ConvertInternalMetadata(UK.Gov.Legislation.Judgments.AkomaNtoso.Meta meta) {
-        return new Meta() {
+    internal static Meta ConvertInternalMetadata(AkN.Meta meta)
+    {
+        return new Meta
+        {
             DocumentType = meta.DocElementName,
             Uri = URI.IsEmpty(meta.WorkUri) ? null : meta.WorkUri,
             Court = meta.UKCourt,
             Cite = meta.UKCite,
             Date = meta.WorkDate,
             Name = meta.WorkName,
-            Attachments = meta.ExternalAttachments.Select(a => new ExternalAttachment() { Name = a.ShowAs, Link = a.Href })
+            Attachments =
+                meta.ExternalAttachments.Select(a => new ExternalAttachment { Name = a.ShowAs, Link = a.Href })
         };
     }
 
-    internal static Image ConvertImage(IImage image) {
-        return new Image() {
+    internal static Image ConvertImage(IImage image)
+    {
+        return new Image
+        {
             Name = image.Name,
             Type = image.ContentType,
             Content = image.Read()
         };
     }
 
-    internal static AttachmentPair ConvertAttachment(Attachment a) {
-        var content = a.Content;
-        var type1 = a.Type;
-        UK.Gov.Legislation.Judgments.AttachmentType type2;
-        if (type1 == Api.AttachmentType.Order)
-            type2 = UK.Gov.Legislation.Judgments.AttachmentType.Order;
-        else if (type1 == Api.AttachmentType.Appendix)
-            type2 = UK.Gov.Legislation.Judgments.AttachmentType.Appendix;
-        else
-            throw new System.Exception();
-        return new System.Tuple<byte[], UK.Gov.Legislation.Judgments.AttachmentType>(content, type2);
+    private static Legislation.Judgments.AttachmentType MapAttachmentType(AttachmentType attachmentType)
+    {
+        return attachmentType switch
+        {
+            AttachmentType.Order => Legislation.Judgments.AttachmentType.Order,
+            AttachmentType.Appendix => Legislation.Judgments.AttachmentType.Appendix,
+            _ => throw new ArgumentOutOfRangeException(nameof(attachmentType), attachmentType, null)
+        };
     }
 
-    internal void Log(Api.Meta meta) {
+    internal void Log(Meta meta)
+    {
         if (string.IsNullOrEmpty(meta.DocumentType))
+        {
             logger.LogWarning("The document type is null");
+        }
         else
+        {
             logger.LogInformation("The document type is {DocumentType}", meta.DocumentType);
-        if (string.IsNullOrEmpty(URI.ExtractShortURIComponent(meta.Uri)))
-            logger.LogWarning("The {DocumentType} uri is null", meta.DocumentType);
-        else
-            logger.LogInformation("The {DocumentType} uri is {Uri}", meta.DocumentType, meta.Uri);
-        if (meta.Court is null)
-            logger.LogWarning("The court is null");
-        else
-            logger.LogInformation("The court is {Court}", meta.Court);
-        if (meta.Cite is null)
-            logger.LogWarning("The case citation is null");
-        else
-            logger.LogInformation("The case citation is {Cite}", meta.Cite);
-        if (meta.Date is null)
-            logger.LogWarning("The {DocumentType} date is null", meta.DocumentType);
-        else
-            logger.LogInformation("The {DocumentType} date is {Date}", meta.DocumentType, meta.Date);
-        if (meta.Name is null)
-            logger.LogWarning("The {DocumentType} name is null", meta.DocumentType);
-        else
-            logger.LogInformation("The {DocumentType} name is {Name}", meta.DocumentType, meta.Name);
-    }
+        }
 
+        if (string.IsNullOrEmpty(URI.ExtractShortURIComponent(meta.Uri)))
+        {
+            logger.LogWarning("The {DocumentType} uri is null", meta.DocumentType);
+        }
+        else
+        {
+            logger.LogInformation("The {DocumentType} uri is {Uri}", meta.DocumentType, meta.Uri);
+        }
+
+        if (meta.Court is null)
+        {
+            logger.LogWarning("The court is null");
+        }
+        else
+        {
+            logger.LogInformation("The court is {Court}", meta.Court);
+        }
+
+        if (meta.Cite is null)
+        {
+            logger.LogWarning("The case citation is null");
+        }
+        else
+        {
+            logger.LogInformation("The case citation is {Cite}", meta.Cite);
+        }
+
+        if (meta.Date is null)
+        {
+            logger.LogWarning("The {DocumentType} date is null", meta.DocumentType);
+        }
+        else
+        {
+            logger.LogInformation("The {DocumentType} date is {Date}", meta.DocumentType, meta.Date);
+        }
+
+        if (meta.Name is null)
+        {
+            logger.LogWarning("The {DocumentType} name is null", meta.DocumentType);
+        }
+        else
+        {
+            logger.LogInformation("The {DocumentType} name is {Name}", meta.DocumentType, meta.Name);
+        }
+    }
 }
